6bc0dc733236be993225686bd139c3a103a05fdf
MistressAlison <alisonmoons@gmail.com>
2021-06-11 15:44:18 -0300

    Added new Power to increase Module Slots

---
+++ TheM10Robot/src/main/java/M10Robot/cards/RAMUpgrade.java
@@ -0,0 +1,88 @@
+package M10Robot.cards;
+
+import M10Robot.M10RobotMod;
+import M10Robot.cards.abstractCards.AbstractDynamicCard;
+import M10Robot.cards.interfaces.ModularDescription;
+import M10Robot.characters.M10Robot;
+import M10Robot.powers.ModulesPower;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static M10Robot.M10RobotMod.makeCardPath;
+
+public class RAMUpgrade extends AbstractDynamicCard implements ModularDescription {
+
+    /*
+     * Wiki-page: https://github.com/daviscook477/BaseMod/wiki/Custom-Cards
+     *
+     * Defend Gain 5 (8) block.
+     */
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = M10RobotMod.makeID(RAMUpgrade.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderPower.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.POWER;
+    public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int EFFECT = 1;
+    private static final int UPGRADE_COST = 0;
+
+    // /STAT DECLARATION/
+
+
+    public RAMUpgrade() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        magicNumber = baseMagicNumber = EFFECT;
+        changeDescription();
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new ApplyPowerAction(p, p, new ModulesPower(p, magicNumber)));
+    }
+
+    @Override
+    public void applyPowers() {
+        super.applyPowers();
+        changeDescription();
+    }
+
+    @Override
+    public void triggerOnGlowCheck() {
+        super.triggerOnGlowCheck();
+        changeDescription();
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBaseCost(UPGRADE_COST);
+            initializeDescription();
+        }
+    }
+
+    @Override
+    public void changeDescription() {
+        if (magicNumber > 1) {
+            rawDescription = UPGRADE_DESCRIPTION;
+        } else {
+            rawDescription = DESCRIPTION;
+        }
+        initializeDescription();
+    }
+}
