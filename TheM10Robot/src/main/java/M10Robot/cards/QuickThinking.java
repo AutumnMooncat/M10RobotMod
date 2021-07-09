3f556a3abfb714bdc319eab9cbb30fa54e32f105
MistressAlison <alisonmoons@gmail.com>
2021-07-09 14:22:58 -0300

    Added Quick Thinking - Draws a card and channels a Bit

---
+++ TheM10Robot/src/main/java/M10Robot/cards/QuickThinking.java
@@ -0,0 +1,71 @@
+package M10Robot.cards;
+
+import M10Robot.M10RobotMod;
+import M10Robot.cards.abstractCards.AbstractDynamicCard;
+import M10Robot.cards.interfaces.ModularDescription;
+import M10Robot.characters.M10Robot;
+import M10Robot.orbs.BitOrb;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.defect.ChannelAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static M10Robot.M10RobotMod.makeCardPath;
+
+public class QuickThinking extends AbstractDynamicCard implements ModularDescription {
+
+    // TEXT DECLARATION
+
+    public static final String ID = M10RobotMod.makeID(QuickThinking.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderSkill.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int DRAW = 1;
+    private static final int UPGRADE_PLUS_DRAW = 1;
+
+    // /STAT DECLARATION/
+
+
+    public QuickThinking() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        magicNumber = baseMagicNumber = DRAW;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new DrawCardAction(magicNumber));
+        this.addToBot(new ChannelAction(new BitOrb()));
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_PLUS_DRAW);
+            initializeDescription();
+        }
+    }
+
+    @Override
+    public void changeDescription() {
+        if (DESCRIPTION != null) {
+            if (magicNumber > 1) {
+                rawDescription = UPGRADE_DESCRIPTION;
+            } else {
+                rawDescription = DESCRIPTION;
+            }
+        }
+    }
+}
