02273a78e5a9f7adff1a18e67bbd5d2a59d3d1fe
MistressAlison <alisonmoons@gmail.com>
2021-07-09 14:22:39 -0300

    Added Repeater - Accepts booster extra effects and applies them to a random enemy at the beginning of the turn

---
+++ TheM10Robot/src/main/java/M10Robot/cards/modules/Repeater.java
@@ -0,0 +1,57 @@
+package M10Robot.cards.modules;
+
+import M10Robot.M10RobotMod;
+import M10Robot.cards.abstractCards.AbstractModuleCard;
+import M10Robot.characters.M10Robot;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static M10Robot.M10RobotMod.makeCardPath;
+
+public class Repeater extends AbstractModuleCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = M10RobotMod.makeID(Repeater.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderPower.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.NONE;
+    private static final CardType TYPE = CardType.POWER;
+    public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
+
+    // /STAT DECLARATION/
+
+
+    public Repeater() {
+        super(ID, IMG, TYPE, COLOR, RARITY, TARGET);
+        //This should ensure the card can take any booster
+        damage = baseDamage = block = baseBlock = magicNumber = baseMagicNumber = 1;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {}
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            selfRetain = true;
+            rawDescription = UPGRADE_DESCRIPTION;
+            initializeDescription();
+        }
+    }
+
+    @Override
+    public void onEquip() {}
+
+    @Override
+    public void onRemove() {}
+}
