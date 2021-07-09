7ee90ec14f0c79f28ff85d8cea8fb088b0e3365f
MistressAlison <alisonmoons@gmail.com>
2021-07-09 13:56:18 -0300

    Spectral Cannon damage buffed

--- TheM10Robot/src/main/java/M10Robot/cards/SpectralCannon.java
+++ TheM10Robot/src/main/java/M10Robot/cards/SpectralCannon.java
@@ -39,12 +39,12 @@ public class SpectralCannon extends AbstractReloadableCard {
     // STAT DECLARATION
 
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
-    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
 
     private static final int COST = 1;
-    private static final int DAMAGE = 11;
+    private static final int DAMAGE = 13;
     private static final int UPGRADE_PLUS_DMG = 4;
     private static final int SHOTS = 1;
 
