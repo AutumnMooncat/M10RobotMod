427f3c61c0e93e12a06591cf7559485c8a1f4c9d
MistressAlison <alisonmoons@gmail.com>
2021-07-09 14:34:03 -0300

    Added Filter - Exhaust any number of cards and gain energy for each exhausted

---
+++ TheM10Robot/src/main/java/M10Robot/cards/Filter.java
@@ -0,0 +1,91 @@
+package M10Robot.cards;
+
+import M10Robot.M10RobotMod;
+import M10Robot.cards.abstractCards.AbstractDynamicCard;
+import M10Robot.characters.M10Robot;
+import com.evacipated.cardcrawl.mod.stslib.actions.common.SelectCardsInHandAction;
+import com.evacipated.cardcrawl.mod.stslib.cards.interfaces.BranchingUpgradesCard;
+import com.megacrit.cardcrawl.actions.common.ExhaustSpecificCardAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Consumer;
+
+import static M10Robot.M10RobotMod.makeCardPath;
+
+public class Filter extends AbstractDynamicCard implements BranchingUpgradesCard {
+
+    /*
+     * Wiki-page: https://github.com/daviscook477/BaseMod/wiki/Custom-Cards
+     *
+     * Defend Gain 5 (8) block.
+     */
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = M10RobotMod.makeID(Filter.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderSkill.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int UPGRADE_COST = 0;
+
+    // /STAT DECLARATION/
+
+
+    public Filter() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        Consumer<List<AbstractCard>> plsWork = this::exhaustEnergize;
+        this.addToBot(new SelectCardsInHandAction(99, EXTENDED_DESCRIPTION[0], true, true, Objects::nonNull, plsWork));
+    }
+
+    private void exhaustEnergize(List<AbstractCard> l) {
+        this.addToBot(new GainEnergyAction(l.size()));
+        for (AbstractCard c : l) {
+            this.addToBot(new ExhaustSpecificCardAction(c, AbstractDungeon.player.hand));
+        }
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            if (isBranchUpgrade()) {
+                branchUpgrade();
+            } else {
+                baseUpgrade();
+            }
+            initializeDescription();
+        }
+    }
+
+    public void baseUpgrade() {
+        upgradeBaseCost(UPGRADE_COST);
+    }
+
+    public void branchUpgrade() {
+        this.selfRetain = true;
+        rawDescription = UPGRADE_DESCRIPTION;
+    }
+}
