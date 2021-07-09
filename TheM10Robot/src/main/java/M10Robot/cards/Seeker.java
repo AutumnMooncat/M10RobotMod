3aa8888f278e5b692c520242f50713a7ca1638e7
MistressAlison <alisonmoons@gmail.com>
2021-07-09 14:25:21 -0300

    Added Seeker - Deals damage and channels Searchlight

---
+++ TheM10Robot/src/main/java/M10Robot/cards/Seeker.java
@@ -0,0 +1,65 @@
+package M10Robot.cards;
+
+import M10Robot.M10RobotMod;
+import M10Robot.cards.abstractCards.AbstractDynamicCard;
+import M10Robot.characters.M10Robot;
+import M10Robot.orbs.SearchlightOrb;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.AttackDamageRandomEnemyAction;
+import com.megacrit.cardcrawl.actions.defect.ChannelAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static M10Robot.M10RobotMod.makeCardPath;
+
+public class Seeker extends AbstractDynamicCard {
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = M10RobotMod.makeID(Seeker.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderAttack.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final AbstractCard.CardRarity RARITY = CardRarity.COMMON;
+    private static final AbstractCard.CardTarget TARGET = CardTarget.ALL_ENEMY;
+    private static final AbstractCard.CardType TYPE = CardType.ATTACK;
+    public static final AbstractCard.CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int DAMAGE = 9;
+    private static final int UPGRADE_PLUS_DMG = 3;
+    private static final int ORBS = 1;
+
+    // /STAT DECLARATION/
+
+    public Seeker() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = damage = DAMAGE;
+        magicNumber = baseMagicNumber = ORBS;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new AttackDamageRandomEnemyAction(this, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
+        for (int i = 0 ; i < magicNumber ; i++) {
+            this.addToBot(new ChannelAction(new SearchlightOrb()));
+        }
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            initializeDescription();
+        }
+    }
+}
