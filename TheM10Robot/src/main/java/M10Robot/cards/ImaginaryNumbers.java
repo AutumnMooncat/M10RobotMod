4cdf3806004d138b3fa4257d458f9f88e58470a3
MistressAlison <alisonmoons@gmail.com>
2021-07-09 14:28:36 -0300

    Added Imaginary Numbers - Give large temp Str and temp Dex

---
+++ TheM10Robot/src/main/java/M10Robot/cards/ImaginaryNumbers.java
@@ -0,0 +1,80 @@
+package M10Robot.cards;
+
+import M10Robot.M10RobotMod;
+import M10Robot.cards.abstractCards.AbstractDynamicCard;
+import M10Robot.characters.M10Robot;
+import com.evacipated.cardcrawl.mod.stslib.cards.interfaces.BranchingUpgradesCard;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.DexterityPower;
+import com.megacrit.cardcrawl.powers.LoseDexterityPower;
+import com.megacrit.cardcrawl.powers.LoseStrengthPower;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+
+import static M10Robot.M10RobotMod.makeCardPath;
+
+public class ImaginaryNumbers extends AbstractDynamicCard implements BranchingUpgradesCard {
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = M10RobotMod.makeID(ImaginaryNumbers.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderSkill.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.ALL;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int UPGRADE_COST = 0;
+    private static final int EFFECT = 7;
+
+    // /STAT DECLARATION/
+
+
+    public ImaginaryNumbers() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        magicNumber = baseMagicNumber = EFFECT;
+        secondMagicNumber = baseSecondMagicNumber = EFFECT;
+        this.exhaust = true;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new ApplyPowerAction(p, p, new StrengthPower(p, magicNumber)));
+        this.addToBot(new ApplyPowerAction(p, p, new LoseStrengthPower(p, secondMagicNumber)));
+        this.addToBot(new ApplyPowerAction(p, p, new DexterityPower(p, magicNumber)));
+        this.addToBot(new ApplyPowerAction(p, p, new LoseDexterityPower(p, secondMagicNumber)));
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            if (isBranchUpgrade()) {
+                branchUpgrade();
+            } else {
+                baseUpgrade();
+            }
+            initializeDescription();
+        }
+    }
+
+    public void baseUpgrade() {
+        upgradeBaseCost(UPGRADE_COST);
+    }
+
+    public void branchUpgrade() {
+        this.selfRetain = true;
+        rawDescription = UPGRADE_DESCRIPTION;
+    }
+}
