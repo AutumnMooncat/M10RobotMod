516866547680a67649862eaf407113ea69406c65
MistressAlison <alisonmoons@gmail.com>
2021-07-09 14:13:31 -0300

    Added Assembly line as the ultimate Preset channeling card. No other cards actually channel Present yet so it is technically the ultimate and only Present card.

---
+++ TheM10Robot/src/main/java/M10Robot/cards/AssemblyLine.java
@@ -0,0 +1,62 @@
+package M10Robot.cards;
+
+import M10Robot.M10RobotMod;
+import M10Robot.cards.abstractCards.AbstractDynamicCard;
+import M10Robot.characters.M10Robot;
+import M10Robot.orbs.PresentOrb;
+import com.megacrit.cardcrawl.actions.defect.ChannelAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static M10Robot.M10RobotMod.makeCardPath;
+
+public class AssemblyLine extends AbstractDynamicCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = M10RobotMod.makeID(AssemblyLine.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderSkill.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
+
+    private static final int COST = 2;
+    private static final int UPGRADE_COST = 1;
+    private static final int ORBS = 3;
+    //private static final int UPGRADE_PLUS_ORBS = 1;
+
+    // /STAT DECLARATION/
+
+
+    public AssemblyLine() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        magicNumber = baseMagicNumber = ORBS;
+        exhaust = true;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        for (int i = 0 ; i < magicNumber ; i++) {
+            this.addToBot(new ChannelAction(new PresentOrb()));
+        }
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBaseCost(UPGRADE_COST);
+            //upgradeMagicNumber(UPGRADE_PLUS_ORBS);
+            initializeDescription();
+        }
+    }
+}
