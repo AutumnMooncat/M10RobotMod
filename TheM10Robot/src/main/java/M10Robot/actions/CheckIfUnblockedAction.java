8a720468d83c101bfb2d115071a1c0f696e4a156
MistressAlison <alisonmoons@gmail.com>
2021-07-09 13:50:54 -0300

    Added a new action that checks if the enemy took damage, then runs the provided action if it did

---
+++ TheM10Robot/src/main/java/M10Robot/actions/CheckIfUnblockedAction.java
@@ -0,0 +1,26 @@
+package M10Robot.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.Settings;
+
+public class CheckIfUnblockedAction extends AbstractGameAction {
+
+    private final AbstractCreature source;
+    private final AbstractGameAction action;
+
+    public CheckIfUnblockedAction(AbstractCreature target, AbstractCreature source, AbstractGameAction action) {
+        this.actionType = ActionType.HEAL;
+        this.target = target;
+        this.source = source;
+        this.action = action;
+        this.duration = Settings.ACTION_DUR_XFAST;
+    }
+
+    public void update() {
+        if (target.lastDamageTaken > 0) {
+            this.addToTop(action);
+        }
+        this.isDone = true;
+    }
+}
