c85030f4a94137a2801d16795fadf52eb396d245
MistressAlison <alisonmoons@gmail.com>
2021-07-09 14:29:02 -0300

    Added Health Down - Enemies lose HP and you gain Temp HP equal in equal amounts

---
+++ TheM10Robot/src/main/java/M10Robot/cards/HealthDown.java
@@ -0,0 +1,71 @@
+package M10Robot.cards;
+
+import M10Robot.M10RobotMod;
+import M10Robot.actions.FasterLoseHPAction;
+import M10Robot.cards.abstractCards.AbstractDynamicCard;
+import M10Robot.characters.M10Robot;
+import com.evacipated.cardcrawl.mod.stslib.actions.tempHp.AddTemporaryHPAction;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static M10Robot.M10RobotMod.makeCardPath;
+
+public class HealthDown extends AbstractDynamicCard {
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = M10RobotMod.makeID(HealthDown.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderSkill.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.ALL;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int EFFECT = 4;
+    private static final int UPGRADE_PLUS_EFFECT = 2;
+
+    // /STAT DECLARATION/
+
+
+    public HealthDown() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        magicNumber = baseMagicNumber = EFFECT;
+        this.exhaust = true;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        int sum = 0, reduceAmount;
+        for (AbstractMonster aM : AbstractDungeon.getMonsters().monsters) {
+            if (!aM.isDeadOrEscaped()) {
+                reduceAmount = Math.min(magicNumber, aM.currentHealth);
+                this.addToBot(new FasterLoseHPAction(aM, p, reduceAmount, AbstractGameAction.AttackEffect.FIRE));
+                sum += reduceAmount;
+            }
+        }
+        if (sum > 0) {
+            this.addToBot(new AddTemporaryHPAction(p, p, sum));
+        }
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_PLUS_EFFECT);
+            initializeDescription();
+        }
+    }
+}
