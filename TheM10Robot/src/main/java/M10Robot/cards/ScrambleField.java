23eb79e61142577c77464a9ca9897d672332b1a8
MistressAlison <alisonmoons@gmail.com>
2021-07-09 13:36:38 -0300

    Scramble Field now uses NegativePrimary interface

--- TheM10Robot/src/main/java/M10Robot/cards/ScrambleField.java
+++ TheM10Robot/src/main/java/M10Robot/cards/ScrambleField.java
@@ -2,15 +2,17 @@ package M10Robot.cards;
 
 import M10Robot.M10RobotMod;
 import M10Robot.cards.abstractCards.AbstractDynamicCard;
+import M10Robot.cards.interfaces.NegativePrimaryEffect;
 import M10Robot.characters.M10Robot;
 import M10Robot.powers.ScrambleFieldPower;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static M10Robot.M10RobotMod.makeCardPath;
 
-public class ScrambleField extends AbstractDynamicCard {
+public class ScrambleField extends AbstractDynamicCard implements NegativePrimaryEffect {
 
     /*
      * Wiki-page: https://github.com/daviscook477/BaseMod/wiki/Custom-Cards
@@ -37,19 +39,46 @@ public class ScrambleField extends AbstractDynamicCard {
     private static final int COST = 2;
     private static final int EFFECT = 7;
     private static final int UPGRADE_PLUS_EFFECT = -2;
-
+    private static final int OFFSET = 1;
     // /STAT DECLARATION/
 
 
     public ScrambleField() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
-        magicNumber = baseMagicNumber = EFFECT;
+        magicNumber = baseMagicNumber = OFFSET;
+        secondMagicNumber = baseSecondMagicNumber = EFFECT;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new ApplyPowerAction(m, p, new ScrambleFieldPower(m, magicNumber)));
+        this.addToBot(new ApplyPowerAction(m, p, new ScrambleFieldPower(m, secondMagicNumber)));
+    }
+
+    @Override
+    public void applyPowers() {
+        super.applyPowers();
+        doMagicNumberShenanigans();
+    }
+
+    @Override
+    public void calculateCardDamage(AbstractMonster mo) {
+        super.calculateCardDamage(mo);
+        doMagicNumberShenanigans();
+    }
+
+    private void doMagicNumberShenanigans() {
+        if (AbstractDungeon.player != null && !AbstractDungeon.isScreenUp) {
+            int delta = magicNumber - OFFSET;
+            secondMagicNumber = EFFECT + (upgraded ? UPGRADE_PLUS_EFFECT : 0);
+            baseSecondMagicNumber = EFFECT + (upgraded ? UPGRADE_PLUS_EFFECT : 0);
+            if (delta > 0) {
+                //time for some shenanigans
+                secondMagicNumber = Math.max(secondMagicNumber-delta,0);
+                baseSecondMagicNumber = Math.max(baseSecondMagicNumber-delta,0);
+            }
+            isSecondMagicNumberModified = secondMagicNumber != baseSecondMagicNumber;
+        }
     }
 
     //Upgraded stats.
@@ -58,7 +87,13 @@ public class ScrambleField extends AbstractDynamicCard {
         if (!upgraded) {
             upgradeName();
             upgradeMagicNumber(UPGRADE_PLUS_EFFECT);
+            upgradeSecondMagicNumber(UPGRADE_PLUS_EFFECT);
             initializeDescription();
         }
+    }
+
+    @Override
+    public boolean reboundOnNegative() {
+        return false;
     }
 }
