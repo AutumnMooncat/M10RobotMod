e966c7b2ffe624fadcae4784922b103610791229
MistressAlison <alisonmoons@gmail.com>
2021-07-09 14:36:15 -0300

    Added Turbo and accompanying power - Draw 1 card and gain 1 energy at the beginning of your next few turns

---
+++ TheM10Robot/src/main/java/M10Robot/cards/Turbo.java
@@ -0,0 +1,72 @@
+package M10Robot.cards;
+
+import M10Robot.M10RobotMod;
+import M10Robot.cards.abstractCards.AbstractDynamicCard;
+import M10Robot.characters.M10Robot;
+import M10Robot.powers.TurboPower;
+import com.evacipated.cardcrawl.mod.stslib.cards.interfaces.BranchingUpgradesCard;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static M10Robot.M10RobotMod.makeCardPath;
+
+public class Turbo extends AbstractDynamicCard implements BranchingUpgradesCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = M10RobotMod.makeID(Turbo.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderSkill.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int UPGRADE_COST = 0;
+    private static final int TURNS = 2;
+    private static final int UPGRADE_PLUS_TURNS = 1;
+
+    // /STAT DECLARATION/
+
+
+    public Turbo() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        magicNumber = baseMagicNumber = TURNS;
+        this.exhaust = true;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new ApplyPowerAction(p, p, new TurboPower(p, magicNumber)));
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            if (isBranchUpgrade()) {
+                branchUpgrade();
+            } else {
+                baseUpgrade();
+            }
+            initializeDescription();
+        }
+    }
+
+    public void baseUpgrade() {
+        upgradeBaseCost(UPGRADE_COST);
+    }
+
+    public void branchUpgrade() {
+        upgradeMagicNumber(UPGRADE_PLUS_TURNS);
+    }
+}
---
+++ TheM10Robot/src/main/java/M10Robot/powers/TurboPower.java
@@ -0,0 +1,72 @@
+package M10Robot.powers;
+
+import M10Robot.M10RobotMod;
+import M10Robot.cards.Turbo;
+import basemod.interfaces.CloneablePowerInterface;
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+public class TurboPower extends AbstractPower implements CloneablePowerInterface {
+
+    public static final String POWER_ID = M10RobotMod.makeID("TurboPower");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
+    // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
+    //private static final Texture tex84 = TextureLoader.getTexture(makePowerPath("placeholder_power84.png"));
+    //private static final Texture tex32 = TextureLoader.getTexture(makePowerPath("placeholder_power32.png"));
+
+    public TurboPower(AbstractCreature owner, int amount) {
+        this.name = NAME;
+        this.ID = POWER_ID;
+        this.owner = owner;
+        this.amount = amount;
+
+        this.type = PowerType.BUFF;
+        this.isTurnBased = true;
+
+        // We load those txtures here.
+        //this.loadRegion("cExplosion");
+        this.loadRegion("draw");
+        //this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        //this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+
+        updateDescription();
+    }
+
+    public void onInitialApplication() {
+        ++AbstractDungeon.player.gameHandSize;
+    }
+
+    public void onEnergyRecharge() {
+        this.flash();
+        AbstractDungeon.player.gainEnergy(1);
+        this.addToBot(new ReducePowerAction(this.owner, this.owner, this, 1));
+    }
+
+    public void onRemove() {
+        --AbstractDungeon.player.gameHandSize;
+    }
+
+    public void updateDescription() {
+        if (this.amount == 1) {
+            this.description = DESCRIPTIONS[0];
+        } else {
+            this.description = DESCRIPTIONS[1] + this.amount + DESCRIPTIONS[2];
+        }
+
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new TurboPower(owner, amount);
+    }
+
+}
