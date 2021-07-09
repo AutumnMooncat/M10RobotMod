0fa301cf3c7f2d0c1a9c4dfe7555fac657ee75a2
MistressAlison <alisonmoons@gmail.com>
2021-07-09 13:42:51 -0300

    Added a few other unfinished Orb ideas for testing purposes

---
+++ TheM10Robot/src/main/java/M10Robot/orbs/ArmorOrb.java
@@ -0,0 +1,128 @@
+package M10Robot.orbs;
+
+import M10Robot.M10RobotMod;
+import M10Robot.powers.RepairableArmorPower;
+import M10Robot.util.TextureLoader;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.megacrit.cardcrawl.actions.common.*;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.OrbStrings;
+import com.megacrit.cardcrawl.orbs.AbstractOrb;
+import com.megacrit.cardcrawl.powers.DexterityPower;
+import com.megacrit.cardcrawl.vfx.combat.FrostOrbActivateEffect;
+import com.megacrit.cardcrawl.vfx.combat.FrostOrbPassiveEffect;
+
+import static M10Robot.M10RobotMod.makeOrbPath;
+
+public class ArmorOrb extends AbstractCustomOrb {
+
+    // Standard ID/Description
+    public static final String ORB_ID = M10RobotMod.makeID("ArmorOrb");
+    private static final OrbStrings orbString = CardCrawlGame.languagePack.getOrbString(ORB_ID);
+    public static final String[] DESC = orbString.DESCRIPTION;
+
+    private static final Texture IMG = TextureLoader.getTexture(makeOrbPath("default_orb.png"));
+    // Animation Rendering Numbers - You can leave these at default, or play around with them and see what they change.
+    private float vfxTimer = 1.0f;
+    private float vfxIntervalMin = 0.1f;
+    private float vfxIntervalMax = 0.4f;
+    private static final float ORB_WAVY_DIST = 0.04f;
+    private static final float PI_4 = 12.566371f;
+
+    boolean alreadyRemoved = false;
+
+    public ArmorOrb() {
+
+        ID = ORB_ID;
+        name = orbString.NAME;
+        img = IMG;
+
+        evokeAmount = baseEvokeAmount = 2;
+        passiveAmount = basePassiveAmount = 2;
+
+        updateDescription();
+
+        angle = MathUtils.random(360.0f); // More Animation-related Numbers
+        channelAnimTimer = 0.5f;
+    }
+
+    @Override
+    public void updateDescription() { // Set the on-hover description of the orb
+        applyFocus(); // Apply Focus (Look at the next method)
+        description = DESC[0] + passiveAmount + DESC[1] + DESC[2] + evokeAmount + DESC[3]; // Set the description
+    }
+
+    @Override
+    public void applyFocus() {
+        int oldPassive = passiveAmount;
+        super.applyFocus();
+        int delta = passiveAmount - oldPassive;
+        if (delta > 0) {
+            this.addToTop(new ApplyPowerAction(p, p, new DexterityPower(p, delta)));
+        } else if (delta < 0) {
+            this.addToTop(new ReducePowerAction(p, p, DexterityPower.POWER_ID, -delta));
+        }
+    }
+
+    @Override
+    public void onEvoke() { // 1.On Orb Evoke
+        if (evokeAmount > 0) {
+            this.addToTop(new ApplyPowerAction(p, p, new RepairableArmorPower(p, evokeAmount)));
+        }
+        if (passiveAmount != 0) {
+            this.addToTop(new ReducePowerAction(p, p, DexterityPower.POWER_ID, passiveAmount));
+        }
+    }
+
+    @Override
+    public void onChannel() {
+        this.addToTop(new ApplyPowerAction(p, p, new DexterityPower(p, passiveAmount)));
+    }
+
+    @Override
+    public void updateAnimation() {// You can totally leave this as is.
+        // If you want to create a whole new orb effect - take a look at conspire's Water Orb. It includes a custom sound, too!
+        super.updateAnimation();
+        angle += Gdx.graphics.getDeltaTime() * 45.0f;
+        vfxTimer -= Gdx.graphics.getDeltaTime();
+        if (vfxTimer < 0.0f) {
+            AbstractDungeon.effectList.add(new FrostOrbPassiveEffect(cX, cY)); // This is the purple-sparkles in the orb. You can change this to whatever fits your orb.
+            vfxTimer = MathUtils.random(vfxIntervalMin, vfxIntervalMax);
+        }
+    }
+
+    // Render the orb.
+    @Override
+    public void render(SpriteBatch sb) {
+        sb.setColor(new Color(1.0f, 1.0f, 1.0f, c.a / 2.0f));
+        sb.draw(img, cX - 48.0f, cY - 48.0f + bobEffect.y, 48.0f, 48.0f, 96.0f, 96.0f, scale + MathUtils.sin(angle / PI_4) * ORB_WAVY_DIST * Settings.scale, scale, angle, 0, 0, 96, 96, false, false);
+        sb.setColor(new Color(1.0f, 1.0f, 1.0f, this.c.a / 2.0f));
+        sb.setBlendFunction(770, 1);
+        sb.draw(img, cX - 48.0f, cY - 48.0f + bobEffect.y, 48.0f, 48.0f, 96.0f, 96.0f, scale, scale + MathUtils.sin(angle / PI_4) * ORB_WAVY_DIST * Settings.scale, -angle, 0, 0, 96, 96, false, false);
+        sb.setBlendFunction(770, 771);
+        renderText(sb);
+        hb.render(sb);
+    }
+
+
+    @Override
+    public void triggerEvokeAnimation() { // The evoke animation of this orb is the dark-orb activation effect.
+        AbstractDungeon.effectsQueue.add(new FrostOrbActivateEffect(cX, cY));
+    }
+
+    @Override
+    public void playChannelSFX() { // When you channel this orb, the ATTACK_FIRE effect plays ("Fwoom").
+        CardCrawlGame.sound.play("ATTACK_FIRE", 0.1f);
+    }
+
+    @Override
+    public AbstractOrb makeCopy() {
+        return new ArmorOrb();
+    }
+}
---
+++ TheM10Robot/src/main/java/M10Robot/orbs/TungstenOrb.java
@@ -0,0 +1,145 @@
+package M10Robot.orbs;
+
+import M10Robot.M10RobotMod;
+import M10Robot.util.TextureLoader;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.actions.defect.DecreaseMaxOrbAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.OrbStrings;
+import com.megacrit.cardcrawl.orbs.AbstractOrb;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.MetallicizePower;
+import com.megacrit.cardcrawl.vfx.combat.DarkOrbActivateEffect;
+import com.megacrit.cardcrawl.vfx.combat.DarkOrbPassiveEffect;
+import com.megacrit.cardcrawl.vfx.combat.OrbFlareEffect;
+
+import static M10Robot.M10RobotMod.makeOrbPath;
+
+public class TungstenOrb extends AbstractCustomOrb {
+
+    // Standard ID/Description
+    public static final String ORB_ID = M10RobotMod.makeID("TungstenOrb");
+    private static final OrbStrings orbString = CardCrawlGame.languagePack.getOrbString(ORB_ID);
+    public static final String[] DESC = orbString.DESCRIPTION;
+
+    private static final Texture IMG = TextureLoader.getTexture(makeOrbPath("default_orb.png"));
+    // Animation Rendering Numbers - You can leave these at default, or play around with them and see what they change.
+    private float vfxTimer = 1.0f;
+    private float vfxIntervalMin = 0.1f;
+    private float vfxIntervalMax = 0.4f;
+    private static final float ORB_WAVY_DIST = 0.04f;
+    private static final float PI_4 = 12.566371f;
+
+    public TungstenOrb() {
+
+        ID = ORB_ID;
+        name = orbString.NAME;
+        img = IMG;
+
+        linkedPower = new TungstenOrbPower(this);
+
+        evokeAmount = baseEvokeAmount = 3;
+        passiveAmount = basePassiveAmount = 2;
+
+        updateDescription();
+
+        angle = MathUtils.random(360.0f); // More Animation-related Numbers
+        channelAnimTimer = 0.5f;
+    }
+
+    @Override
+    public void updateDescription() { // Set the on-hover description of the orb
+        applyFocus(); // Apply Focus (Look at the next method)
+        description = DESC[0] + passiveAmount + DESC[1] + DESC[2] + evokeAmount + DESC[3]; // Set the description
+    }
+
+    @Override
+    public void onEvoke() { // 1.On Orb Evoke
+        this.addToBot(new ApplyPowerAction(p, p, new MetallicizePower(p, evokeAmount)));
+        //this.addToBot(new DamageAllEnemiesAction(p, DamageInfo.createDamageMatrix(evokeAmount, true, true), DamageInfo.DamageType.THORNS, AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
+        this.addToBot(new RemoveSpecificPowerAction(p, p, linkedPower));
+        this.addToBot(new DecreaseMaxOrbAction(1));
+    }
+
+    @Override
+    public void onChannel() {
+        this.addToBot(new ApplyPowerAction(p, p, linkedPower));
+    }
+
+    @Override
+    public void onLinkedPowerTrigger() {
+        this.addToTop(new VFXAction(new OrbFlareEffect(this, OrbFlareEffect.OrbFlareColor.FROST), 0.0f));
+    }
+
+    @Override
+    public void updateAnimation() {// You can totally leave this as is.
+        // If you want to create a whole new orb effect - take a look at conspire's Water Orb. It includes a custom sound, too!
+        super.updateAnimation();
+        angle += Gdx.graphics.getDeltaTime() * 45.0f;
+        vfxTimer -= Gdx.graphics.getDeltaTime();
+        if (vfxTimer < 0.0f) {
+            AbstractDungeon.effectList.add(new DarkOrbPassiveEffect(cX, cY)); // This is the purple-sparkles in the orb. You can change this to whatever fits your orb.
+            vfxTimer = MathUtils.random(vfxIntervalMin, vfxIntervalMax);
+        }
+    }
+
+    // Render the orb.
+    @Override
+    public void render(SpriteBatch sb) {
+        sb.setColor(new Color(1.0f, 1.0f, 1.0f, c.a / 2.0f));
+        sb.draw(img, cX - 48.0f, cY - 48.0f + bobEffect.y, 48.0f, 48.0f, 96.0f, 96.0f, scale + MathUtils.sin(angle / PI_4) * ORB_WAVY_DIST * Settings.scale, scale, angle, 0, 0, 96, 96, false, false);
+        sb.setColor(new Color(1.0f, 1.0f, 1.0f, this.c.a / 2.0f));
+        sb.setBlendFunction(770, 1);
+        sb.draw(img, cX - 48.0f, cY - 48.0f + bobEffect.y, 48.0f, 48.0f, 96.0f, 96.0f, scale, scale + MathUtils.sin(angle / PI_4) * ORB_WAVY_DIST * Settings.scale, -angle, 0, 0, 96, 96, false, false);
+        sb.setBlendFunction(770, 771);
+        renderText(sb);
+        hb.render(sb);
+    }
+
+
+    @Override
+    public void triggerEvokeAnimation() { // The evoke animation of this orb is the dark-orb activation effect.
+        AbstractDungeon.effectsQueue.add(new DarkOrbActivateEffect(cX, cY));
+    }
+
+    @Override
+    public void playChannelSFX() { // When you channel this orb, the ATTACK_FIRE effect plays ("Fwoom").
+        CardCrawlGame.sound.play("ATTACK_FIRE", 0.1f);
+    }
+
+    @Override
+    public AbstractOrb makeCopy() {
+        return new TungstenOrb();
+    }
+
+    private static class TungstenOrbPower extends AbstractLinkedOrbPower {
+
+        public TungstenOrbPower(AbstractCustomOrb linkedOrb) {
+            super(linkedOrb);
+        }
+
+        @Override
+        public int onAttackedToChangeDamage(DamageInfo info, int damageAmount) {
+            if (damageAmount > 0) {
+                damageAmount = Math.max(0, damageAmount - linkedOrb.passiveAmount);
+                linkedOrb.onLinkedPowerTrigger();
+            }
+            return damageAmount;
+        }
+
+        @Override
+        public AbstractPower makeCopy() {
+            return new TungstenOrbPower(linkedOrb);
+        }
+    }
+}
