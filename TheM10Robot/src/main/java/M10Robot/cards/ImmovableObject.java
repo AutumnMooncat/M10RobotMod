839c7f901251f2dba1b84d9bfcf8c6c56025fb79
MistressAlison <alisonmoons@gmail.com>
2021-07-09 14:27:22 -0300

    Added Immovable Object - Huge Block card that is 3 cost

---
+++ TheM10Robot/src/main/java/M10Robot/cards/ImmovableObject.java
@@ -0,0 +1,63 @@
+package M10Robot.cards;
+
+import M10Robot.M10RobotMod;
+import M10Robot.actions.RepairArmorAction;
+import M10Robot.cards.abstractCards.AbstractDynamicCard;
+import M10Robot.characters.M10Robot;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static M10Robot.M10RobotMod.makeCardPath;
+
+public class ImmovableObject extends AbstractDynamicCard {
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = M10RobotMod.makeID(ImmovableObject.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderSkill.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
+
+    private static final int COST = 3;
+    private static final int BLOCK = 25;
+    private static final int UPGRADE_PLUS_BLOCK = 10;
+    private static final int REPAIR = 7;
+    private static final int UPGRADE_PLUS_REPAIR = 3;
+
+    // /STAT DECLARATION/
+
+
+    public ImmovableObject() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        block = baseBlock = BLOCK;
+        //magicNumber = baseMagicNumber = REPAIR;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new GainBlockAction(p, p, block));
+        //this.addToBot(new RepairArmorAction(p, magicNumber));
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBlock(UPGRADE_PLUS_BLOCK);
+            upgradeMagicNumber(UPGRADE_PLUS_REPAIR);
+            initializeDescription();
+        }
+    }
+}
