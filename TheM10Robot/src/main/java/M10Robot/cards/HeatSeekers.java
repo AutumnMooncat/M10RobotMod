e1fd6e1b023b6df553774b6c0c4343a8f04a4d3d
MistressAlison <alisonmoons@gmail.com>
2021-07-12 09:49:47 -0300

    Added Thermal Imaging / Heat Seekers and accompanying images - Applies Lock-On to ALL enemies / Deals damage to ALL enemies with Lock-On

---
+++ TheM10Robot/src/main/java/M10Robot/cards/HeatSeekers.java
@@ -0,0 +1,68 @@
+package M10Robot.cards;
+
+import M10Robot.M10RobotMod;
+import M10Robot.cards.abstractCards.AbstractDynamicCard;
+import M10Robot.cards.abstractCards.AbstractSwappableCard;
+import M10Robot.cards.uniqueCards.UniqueCard;
+import M10Robot.characters.M10Robot;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.LockOnPower;
+
+import static M10Robot.M10RobotMod.makeCardPath;
+
+public class HeatSeekers extends AbstractSwappableCard implements UniqueCard {
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = M10RobotMod.makeID(HeatSeekers.class.getSimpleName());
+    public static final String IMG = makeCardPath("HeatSeekers.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
+
+    private static final int COST = 0;
+    private static final int DAMAGE = 4;
+    private static final int UPGRADE_PLUS_DMG = 2;
+
+    // /STAT DECLARATION/
+
+    public HeatSeekers() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = damage = DAMAGE;
+        this.isMultiDamage = true;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        for (AbstractMonster aM : AbstractDungeon.getMonsters().monsters) {
+            if (!aM.isDeadOrEscaped() && aM.hasPower(LockOnPower.POWER_ID)) {
+                this.addToBot(new DamageAction(aM, new DamageInfo(p, multiDamage[AbstractDungeon.getMonsters().monsters.indexOf(aM)], damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE, true));
+            }
+        }
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            initializeDescription();
+            super.upgrade();
+        }
+    }
+}
---
+++ TheM10Robot/src/main/java/M10Robot/cards/ThermalImaging.java
@@ -0,0 +1,60 @@
+package M10Robot.cards;
+
+import M10Robot.M10RobotMod;
+import M10Robot.cards.abstractCards.AbstractSwappableCard;
+import M10Robot.characters.M10Robot;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.LockOnPower;
+
+import static M10Robot.M10RobotMod.makeCardPath;
+
+public class ThermalImaging extends AbstractSwappableCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = M10RobotMod.makeID(ThermalImaging.class.getSimpleName());
+    public static final String IMG = makeCardPath("ThermalImaging.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int EFFECT = 2;
+    private static final int UPGRADE_PLUS_EFFECT = 1;
+
+    // /STAT DECLARATION/
+
+    public ThermalImaging() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET, new HeatSeekers());
+        magicNumber = baseMagicNumber = EFFECT;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        for (AbstractMonster aM : AbstractDungeon.getMonsters().monsters) {
+            this.addToBot(new ApplyPowerAction(aM, p, new LockOnPower(aM, magicNumber)));
+        }
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_PLUS_EFFECT);
+            initializeDescription();
+            super.upgrade();
+        }
+    }
+}
