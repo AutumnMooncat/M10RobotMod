24d09a7e1ce3551a4f0530685ae0a2e91a6984dc
MistressAlison <alisonmoons@gmail.com>
2021-07-12 09:50:43 -0300

    Added Compile Data - Skyson's very strong card

---
+++ TheM10Robot/src/main/java/M10Robot/cards/CompileData.java
@@ -0,0 +1,75 @@
+package M10Robot.cards;
+
+import M10Robot.M10RobotMod;
+import M10Robot.cards.abstractCards.AbstractDynamicCard;
+import M10Robot.cards.interfaces.ModularDescription;
+import M10Robot.characters.M10Robot;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.utility.ScryAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static M10Robot.M10RobotMod.makeCardPath;
+
+public class CompileData extends AbstractDynamicCard implements ModularDescription {
+
+    // TEXT DECLARATION
+
+    public static final String ID = M10RobotMod.makeID(CompileData.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderSkill.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int UPGRADE_COST = 0;
+    private static final int CARDS = 1;
+    private static final int UPGRADE_PLUS_CARDS = 1;
+
+    // /STAT DECLARATION/
+
+
+    public CompileData() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        magicNumber = baseMagicNumber = CARDS;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        int relics = p.relics.size();
+        if (relics > 0) {
+            this.addToBot(new ScryAction(relics));
+        }
+        this.addToBot(new DrawCardAction(magicNumber));
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBaseCost(UPGRADE_COST);
+            initializeDescription();
+        }
+    }
+
+    @Override
+    public void changeDescription() {
+        if (DESCRIPTION != null) {
+            if (magicNumber > 1) {
+                rawDescription = UPGRADE_DESCRIPTION;
+            } else {
+                rawDescription = DESCRIPTION;
+            }
+        }
+    }
+}
