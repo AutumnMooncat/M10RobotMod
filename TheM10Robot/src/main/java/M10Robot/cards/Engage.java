32ddc56445e88cf050256548a91f677c43cd3806
MistressAlison <alisonmoons@gmail.com>
2021-07-12 09:45:31 -0300

    Added image to Engage

--- TheM10Robot/src/main/java/M10Robot/cards/Engage.java
+++ TheM10Robot/src/main/java/M10Robot/cards/Engage.java
@@ -16,7 +16,7 @@ public class Engage extends AbstractDynamicCard {
     // TEXT DECLARATION
 
     public static final String ID = M10RobotMod.makeID(Engage.class.getSimpleName());
-    public static final String IMG = makeCardPath("PlaceholderSkill.png");
+    public static final String IMG = makeCardPath("Engage.png");
 
     // /TEXT DECLARATION/
 
