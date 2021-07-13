2d8c019495bb97fea2784426b4db91b2d0a9ceb8
MistressAlison <alisonmoons@gmail.com>
2021-07-13 20:09:00 -0300

    Added image to Blank Slate and moved card text to a tooltip

--- TheM10Robot/src/main/java/M10Robot/cards/BlankSlate.java
+++ TheM10Robot/src/main/java/M10Robot/cards/BlankSlate.java
@@ -3,9 +3,13 @@ package M10Robot.cards;
 import M10Robot.M10RobotMod;
 import M10Robot.cards.abstractCards.AbstractDynamicCard;
 import M10Robot.characters.M10Robot;
+import basemod.helpers.TooltipInfo;
 import com.evacipated.cardcrawl.mod.stslib.cards.interfaces.BranchingUpgradesCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static M10Robot.M10RobotMod.makeCardPath;
 
@@ -21,7 +25,8 @@ public class BlankSlate extends AbstractDynamicCard implements BranchingUpgrades
     // TEXT DECLARATION
 
     public static final String ID = M10RobotMod.makeID(BlankSlate.class.getSimpleName());
-    public static final String IMG = makeCardPath("PlaceholderSkill.png");
+    public static final String IMG = makeCardPath("BlankSlate.png");
+    private static ArrayList<TooltipInfo> boosterExplanation;
 
     // /TEXT DECLARATION/
 
@@ -43,6 +48,18 @@ public class BlankSlate extends AbstractDynamicCard implements BranchingUpgrades
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         //This should ensure the card can take any booster
         damage = baseDamage = block = baseBlock = magicNumber = baseMagicNumber = 1;
+    }
+
+    @Override
+    public List<TooltipInfo> getCustomTooltipsTop() {
+        if (boosterExplanation == null)
+        {
+            boosterExplanation = new ArrayList<>();
+            boosterExplanation.add(new TooltipInfo(EXTENDED_DESCRIPTION[0], EXTENDED_DESCRIPTION[1]));
+        }
+        List<TooltipInfo> compoundList = new ArrayList<>(boosterExplanation);
+        if (super.getCustomTooltipsTop() != null) compoundList.addAll(super.getCustomTooltipsTop());
+        return compoundList;
     }
 
     // Actions the card should do.
