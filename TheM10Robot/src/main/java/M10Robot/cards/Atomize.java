2ad4551070e2fbff371e86e08a4ccd4bf78ba542
MistressAlison <alisonmoons@gmail.com>
2021-07-15 09:22:13 -0300

    Added image to Atomize

--- TheM10Robot/src/main/java/M10Robot/cards/Atomize.java
+++ TheM10Robot/src/main/java/M10Robot/cards/Atomize.java
@@ -18,7 +18,7 @@ public class Atomize extends AbstractDynamicCard {
     // TEXT DECLARATION
 
     public static final String ID = M10RobotMod.makeID(Atomize.class.getSimpleName());
-    public static final String IMG = makeCardPath("PlaceholderSkill.png");
+    public static final String IMG = makeCardPath("Atomize.png");
 
     // /TEXT DECLARATION/
 
