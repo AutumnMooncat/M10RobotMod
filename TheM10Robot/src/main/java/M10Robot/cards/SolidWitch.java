727ad61026e26d51479ec88fa2008384b275bfa7
MistressAlison <alisonmoons@gmail.com>
2021-07-15 19:35:57 -0300

    Added Solid Witch - Provides Intangible

---
+++ TheM10Robot/src/main/java/M10Robot/cards/SolidWitch.java
@@ -0,0 +1,58 @@
+package M10Robot.cards;
+
+import M10Robot.M10RobotMod;
+import M10Robot.cards.abstractCards.AbstractDynamicCard;
+import M10Robot.characters.M10Robot;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.IntangiblePlayerPower;
+
+import static M10Robot.M10RobotMod.makeCardPath;
+
+public class SolidWitch extends AbstractDynamicCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = M10RobotMod.makeID(SolidWitch.class.getSimpleName());
+    public static final String IMG = makeCardPath("SolidWitch.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int UPGRADE_COST = 0;
+    private static final int EFFECT = 1;
+
+    // /STAT DECLARATION/
+
+
+    public SolidWitch() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        magicNumber = baseMagicNumber = EFFECT;
+        exhaust = true;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new ApplyPowerAction(p, p, new IntangiblePlayerPower(p, magicNumber)));
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBaseCost(UPGRADE_COST);
+            initializeDescription();
+        }
+    }
+}
