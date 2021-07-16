41eab62d27ab2670655d9d6a915ea823be7ba7df
MistressAlison <alisonmoons@gmail.com>
2021-07-16 18:50:58 -0300

    RepeatFieldPatches now adds an extra copy of the card

--- TheM10Robot/src/main/java/M10Robot/patches/RepeatFieldPatches.java
+++ TheM10Robot/src/main/java/M10Robot/patches/RepeatFieldPatches.java
@@ -1,9 +1,10 @@
 package M10Robot.patches;
 
 import com.evacipated.cardcrawl.modthespire.lib.*;
-import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardQueueItem;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import javassist.CtBehavior;
@@ -21,13 +22,29 @@ public class RepeatFieldPatches {
     }
 
     @SpirePatch(clz = AbstractPlayer.class, method = "useCard")
-    public static class DamageTakenListener {
+    public static class PlayExtraCopies {
         @SpireInsertPatch(locator = Locator.class)
-        public static void damageTakenListener(AbstractPlayer __instance, AbstractCard c, AbstractMonster monster, int energyOnUse) {
+        public static void withoutInfiniteLoopPls(AbstractPlayer __instance, AbstractCard c, AbstractMonster monster, int energyOnUse) {
             if (RepeatFields.repeat.get(c) > 0) {
                 for (int i = 0 ; i < RepeatFields.repeat.get(c) ; i++) {
-                    c.use(__instance, monster);
-                    AbstractDungeon.actionManager.addToBottom(new UseCardAction(c, monster));
+                    AbstractCard tmp = c.makeSameInstanceOf();
+                    AbstractDungeon.player.limbo.addToBottom(tmp);
+                    tmp.current_x = c.current_x;
+                    tmp.current_y = c.current_y;
+                    tmp.target_x = (float) Settings.WIDTH / 2.0F - 300.0F * Settings.scale;
+                    tmp.target_y = (float)Settings.HEIGHT / 2.0F;
+                    if (monster != null) {
+                        tmp.calculateCardDamage(monster);
+                    }
+
+                    tmp.purgeOnUse = true;
+                    RepeatFields.repeat.set(tmp, 0);
+                    AbstractDungeon.actionManager.addCardQueueItem(new CardQueueItem(tmp, monster, c.energyOnUse, true, true), true);
+                    //Bad solution, do not use
+                    /*c.use(__instance, monster);
+                    if (!c.dontTriggerOnUseCard) {
+                        UseCardAction uca = new UseCardAction(c, monster);
+                    }*/
                 }
             }
         }
