6558ffe854c299539a5fe6781d1031db23700eb0
MistressAlison <alisonmoons@gmail.com>
2021-07-17 21:57:14 -0300

    Echo Field Patch now makes Spicy Shops' Repeat modifier work with equipped boosters

--- TheM10Robot/src/main/java/M10Robot/patches/EchoFieldPatches.java
+++ TheM10Robot/src/main/java/M10Robot/patches/EchoFieldPatches.java
@@ -1,5 +1,10 @@
 package M10Robot.patches;
 
+import M10Robot.M10RobotMod;
+import M10Robot.cardModifiers.AbstractExtraEffectModifier;
+import SpicyShops.patches.cards.RepeatKeywordPatches;
+import basemod.abstracts.AbstractCardModifier;
+import basemod.helpers.CardModifierManager;
 import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardQueueItem;
@@ -25,6 +30,15 @@ public class EchoFieldPatches {
     public static class PlayExtraCopies {
         @SpireInsertPatch(locator = Locator.class)
         public static void withoutInfiniteLoopPls(AbstractPlayer __instance, AbstractCard c, AbstractMonster monster, int energyOnUse) {
+            if (M10RobotMod.isSpicyShopsLoaded) {
+                if (RepeatKeywordPatches.RepeatField.repeat.get(c)) {
+                    for (AbstractCardModifier mod : CardModifierManager.modifiers(c)) {
+                        if (mod instanceof AbstractExtraEffectModifier) {
+                            ((AbstractExtraEffectModifier) mod).doExtraEffects(c, __instance, monster);
+                        }
+                    }
+                }
+            }
             if (EchoFields.echo.get(c) > 0) {
                 for (int i = 0; i < EchoFields.echo.get(c) ; i++) {
                     AbstractCard tmp = c.makeSameInstanceOf();
