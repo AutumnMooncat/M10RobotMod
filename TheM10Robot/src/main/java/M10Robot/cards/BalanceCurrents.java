da71c6539225112e07c322d4630953a1ed3ff9a8
MistressAlison <alisonmoons@gmail.com>
2021-07-24 12:14:15 -0300

    Balanced Currents now much faster and has vfx

--- TheM10Robot/src/main/java/M10Robot/cards/BalanceCurrents.java
+++ TheM10Robot/src/main/java/M10Robot/cards/BalanceCurrents.java
@@ -3,15 +3,14 @@ package M10Robot.cards;
 import M10Robot.M10RobotMod;
 import M10Robot.cards.abstractCards.AbstractDynamicCard;
 import M10Robot.characters.M10Robot;
-import M10Robot.powers.ScrambleFieldPower;
 import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.InvisiblePower;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.GainBlockAction;
-import com.megacrit.cardcrawl.actions.common.LoseHPAction;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.vfx.combat.FlashAtkImgEffect;
 
 import static M10Robot.M10RobotMod.makeCardPath;
 
@@ -48,26 +47,39 @@ public class BalanceCurrents extends AbstractDynamicCard {
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        for (AbstractMonster aM : AbstractDungeon.getMonsters().monsters) {
-            if (!aM.isDeadOrEscaped()) {
-                int total = 0, debuff = 0;
-                for (AbstractPower pow : aM.powers) {
-                    //Don't count invisible powers
-                    if (!(pow instanceof InvisiblePower)) {
-                        total++;
-                        if (pow.type == AbstractPower.PowerType.DEBUFF) {
-                            debuff++;
+        this.addToBot(new AbstractGameAction() {
+            @Override
+            public void update() {
+                for (AbstractMonster aM : AbstractDungeon.getMonsters().monsters) {
+                    if (!aM.isDeadOrEscaped()) {
+                        int total = 0, debuff = 0;
+                        for (AbstractPower pow : aM.powers) {
+                            //Don't count invisible powers
+                            if (!(pow instanceof InvisiblePower)) {
+                                total++;
+                                if (pow.type == AbstractPower.PowerType.DEBUFF) {
+                                    debuff++;
+                                }
+                            }
+                        }
+                        if (total > 0) {
+                            AbstractDungeon.effectList.add(new FlashAtkImgEffect(aM.hb.cX, aM.hb.cY, AttackEffect.FIRE));
+                            aM.damage(new DamageInfo(p, magicNumber*total, DamageInfo.DamageType.HP_LOSS));
+
+                        }
+                        if (debuff > 0) {
+                            AbstractDungeon.effectList.add(new FlashAtkImgEffect(aM.hb.cX, aM.hb.cY, AttackEffect.SHIELD));
+                            aM.addBlock(magicNumber*debuff);
                         }
                     }
                 }
-                if (total > 0) {
-                    this.addToBot(new LoseHPAction(aM, p, magicNumber*total));
+                if (AbstractDungeon.getCurrRoom().monsters.areMonstersBasicallyDead()) {
+                    AbstractDungeon.actionManager.clearPostCombatActions();
                 }
-                if (debuff > 0) {
-                    this.addToBot(new GainBlockAction(aM, p, magicNumber*debuff));
-                }
+                this.isDone = true;
             }
-        }
+        });
+
     }
 
     //Upgraded stats.
