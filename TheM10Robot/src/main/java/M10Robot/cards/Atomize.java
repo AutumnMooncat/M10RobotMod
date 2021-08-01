1f8a37260e66f90f20c4f6984d5814fb89bdfc82
MistressAlison <alisonmoons@gmail.com>
2021-08-01 20:31:51 -0300

    Atomize now increases the turns before detonation instead, allowing them to build up additional damage (good for Focus builds)

--- TheM10Robot/src/main/java/M10Robot/cards/Atomize.java
+++ TheM10Robot/src/main/java/M10Robot/cards/Atomize.java
@@ -4,7 +4,7 @@ import M10Robot.M10RobotMod;
 import M10Robot.cards.abstractCards.AbstractDynamicCard;
 import M10Robot.characters.M10Robot;
 import M10Robot.orbs.BombOrb;
-import com.evacipated.cardcrawl.mod.stslib.actions.defect.EvokeSpecificOrbAction;
+import com.evacipated.cardcrawl.mod.stslib.cards.interfaces.BranchingUpgradesCard;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.defect.ChannelAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -13,7 +13,7 @@ import com.megacrit.cardcrawl.orbs.AbstractOrb;
 
 import static M10Robot.M10RobotMod.makeCardPath;
 
-public class Atomize extends AbstractDynamicCard {
+public class Atomize extends AbstractDynamicCard implements BranchingUpgradesCard {
 
     // TEXT DECLARATION
 
@@ -30,10 +30,12 @@ public class Atomize extends AbstractDynamicCard {
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
 
-    private static final int COST = 3;
-    private static final int UPGRADE_COST = 2;
+    private static final int COST = 2;
+    private static final int UPGRADE_COST = 1;
     private static final int ORBS = 2;
+    private static final int TURN_INCREASE = 1;
     //private static final int UPGRADE_PLUS_ORBS = 1;
+    //private static final int UPGRADE_PLUS_TURN_INCREASE = 1;
 
     // /STAT DECLARATION/
 
@@ -41,6 +43,7 @@ public class Atomize extends AbstractDynamicCard {
     public Atomize() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         magicNumber = baseMagicNumber = ORBS;
+        secondMagicNumber = baseSecondMagicNumber = TURN_INCREASE;
         exhaust = true;
     }
 
@@ -55,7 +58,8 @@ public class Atomize extends AbstractDynamicCard {
             public void update() {
                 for (AbstractOrb o : p.orbs) {
                     if (o instanceof BombOrb) {
-                        this.addToTop(new EvokeSpecificOrbAction(o));
+                        ((BombOrb) o).turnCount += secondMagicNumber;
+                        o.updateDescription();
                     }
                 }
                 this.isDone = true;
@@ -68,9 +72,22 @@ public class Atomize extends AbstractDynamicCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeBaseCost(UPGRADE_COST);
-            //upgradeMagicNumber(UPGRADE_PLUS_ORBS);
+            if (isBranchUpgrade()) {
+                branchUpgrade();
+            } else {
+                baseUpgrade();
+            }
             initializeDescription();
         }
+    }
+
+    public void baseUpgrade() {
+        upgradeBaseCost(UPGRADE_COST);
+    }
+
+    public void branchUpgrade() {
+        //upgradeSecondMagicNumber(UPGRADE_PLUS_TURN_INCREASE);
+        this.exhaust = false;
+        rawDescription = UPGRADE_DESCRIPTION;
     }
 }
