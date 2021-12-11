e744ab14edf686600a4850649fddccc21a8c7be3
Alison Turner <alisonmoons@gmail.com>
2021-12-10 23:27:23 -0400

    Added Compensator, reduces recoil at end of turn

--- TheM10Robot/src/main/java/M10Robot/cards/Compensator.java
+++ TheM10Robot/src/main/java/M10Robot/cards/Compensator.java
@@ -1,16 +1,16 @@
-package M10Robot.cutCards;
+package M10Robot.cards;
 
 import M10Robot.M10RobotMod;
 import M10Robot.cards.abstractCards.AbstractDynamicCard;
 import M10Robot.characters.M10Robot;
-import M10Robot.powers.QuickRepairPower;
+import M10Robot.powers.CompensatorPower;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static M10Robot.M10RobotMod.makeCardPath;
 
-public class QuickRepair extends AbstractDynamicCard {
+public class Compensator extends AbstractDynamicCard {
 
     /*
      * Wiki-page: https://github.com/daviscook477/BaseMod/wiki/Custom-Cards
@@ -21,8 +21,8 @@ public class QuickRepair extends AbstractDynamicCard {
 
     // TEXT DECLARATION
 
-    public static final String ID = M10RobotMod.makeID(QuickRepair.class.getSimpleName());
-    public static final String IMG = makeCardPath("PlaceholderPower.png");
+    public static final String ID = M10RobotMod.makeID(Compensator.class.getSimpleName());
+    public static final String IMG = makeCardPath("Compensator.png");
 
     // /TEXT DECLARATION/
 
@@ -30,18 +30,18 @@ public class QuickRepair extends AbstractDynamicCard {
     // STAT DECLARATION
 
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
-    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardTarget TARGET = CardTarget.NONE;
     private static final CardType TYPE = CardType.POWER;
     public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
 
     private static final int COST = 1;
+    private static final int UPGRADE_COST = 0;
     private static final int EFFECT = 1;
-    private static final int UPGRADE_PLUS_EFFECT = 1;
 
     // /STAT DECLARATION/
 
 
-    public QuickRepair() {
+    public Compensator() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         magicNumber = baseMagicNumber = EFFECT;
     }
@@ -49,7 +49,7 @@ public class QuickRepair extends AbstractDynamicCard {
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new ApplyPowerAction(p, p, new QuickRepairPower(p, magicNumber)));
+        this.addToBot(new ApplyPowerAction(p, p, new CompensatorPower(p, magicNumber)));
     }
 
     //Upgraded stats.
@@ -57,7 +57,7 @@ public class QuickRepair extends AbstractDynamicCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeMagicNumber(UPGRADE_PLUS_EFFECT);
+            upgradeBaseCost(UPGRADE_COST);
             initializeDescription();
         }
     }
