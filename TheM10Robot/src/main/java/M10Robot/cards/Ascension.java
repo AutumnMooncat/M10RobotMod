b8ecc8aca505e913b767b650ca9079acbfa1dd41
Alison Turner <alisonmoons@gmail.com>
2021-12-10 23:21:05 -0400

    Ascension now uses Armaments+ action

--- TheM10Robot/src/main/java/M10Robot/cards/Ascension.java
+++ TheM10Robot/src/main/java/M10Robot/cards/Ascension.java
@@ -49,50 +49,7 @@ public class Ascension extends AbstractDynamicCard implements ModularDescription
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         this.addToBot(new DrawCardAction(magicNumber));
-        this.addToBot(new AbstractGameAction() {
-            @Override
-            public void update() {
-                //Map to store if cards were originally upgraded or not
-                HashMap<AbstractCard, Boolean> upgradedMap = new HashMap<>();
-                //Map to store if the card was allowed to upgrade or not
-                HashMap<AbstractCard, Boolean> canUpgradeMap = new HashMap<>();
-                //Loop through all cards first
-                for (AbstractCard c : p.hand.group) {
-                    //Grab the old upgraded variable
-                    upgradedMap.put(c, c.upgraded);
-                    //Grab if the card was allowed to upgrade
-                    canUpgradeMap.put(c, c.canUpgrade());
-                    //Set the card as not upgraded, since that's the check Armaments Action makes
-                    c.upgraded = false;
-                }
-                //Add this to top, since we add the Armaments Action to top, this comes second
-                this.addToTop(new AbstractGameAction() {
-                    @Override
-                    public void update() {
-                        //Loop through all cards again
-                        for (AbstractCard c : p.hand.group) {
-                            //If the card is in our map (it's possible new cards were added)
-                            if (upgradedMap.containsKey(c)) {
-                                //If the card isnt currently upgraded, that means we didnt choose it (since all cards were set as not upgraded)
-                                if (!c.upgraded) {
-                                    //Restore its old upgraded boolean
-                                    c.upgraded = upgradedMap.get(c);
-                                } else { //This is the card that we just upgraded, as its the only one possible to have the upgrade boolean true
-                                    //Check to see if it was originally allowed to upgrade or not (some cards can upgrade more than one time)
-                                    if (!canUpgradeMap.get(c)) {
-                                        //If it wasn't allowed to upgrade before, we forced an upgrade. Save this is a field for use in making copies
-                                        ForcedUpgradeField.wasForced.set(c, true);
-                                    }
-                                }
-                            }
-                        }
-                        this.isDone = true;
-                    }
-                });
-                this.addToTop(new ArmamentsAction(false));
-                this.isDone = true;
-            }
-        });
+        this.addToBot(new ArmamentsAction(true));
     }
 
     //Upgraded stats.
