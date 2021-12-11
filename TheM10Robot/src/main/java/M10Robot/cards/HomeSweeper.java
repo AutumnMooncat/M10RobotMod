840c0e76513d61f39299308729eae1de73cdfef6
Alison Turner <alisonmoons@gmail.com>
2021-12-11 00:17:54 -0400

    Home Sweeper now Aimed and doesnt use subscribers

--- TheM10Robot/src/main/java/M10Robot/cards/HomeSweeper.java
+++ TheM10Robot/src/main/java/M10Robot/cards/HomeSweeper.java
@@ -1,24 +1,23 @@
 package M10Robot.cards;
 
 import M10Robot.M10RobotMod;
+import M10Robot.cardModifiers.AimedModifier;
 import M10Robot.cards.abstractCards.AbstractDynamicCard;
 import M10Robot.characters.M10Robot;
-import basemod.BaseMod;
-import basemod.interfaces.PostEnergyRechargeSubscriber;
+import basemod.helpers.CardModifierManager;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.GameActionManager;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
 import com.megacrit.cardcrawl.actions.utility.SFXAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.vfx.combat.SweepingBeamEffect;
 
 import static M10Robot.M10RobotMod.makeCardPath;
 
-public class HomeSweeper extends AbstractDynamicCard implements PostEnergyRechargeSubscriber {
+public class HomeSweeper extends AbstractDynamicCard {
 
 
     // TEXT DECLARATION
@@ -48,11 +47,8 @@ public class HomeSweeper extends AbstractDynamicCard implements PostEnergyRechar
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         baseDamage = damage = DAMAGE;
         magicNumber = baseMagicNumber = DAMAGE_PER_TURN_SCALING;
-        secondMagicNumber = baseSecondMagicNumber = DAMAGE;
         isMultiDamage = true;
-        if (AbstractDungeon.player != null && !AbstractDungeon.player.masterDeck.contains(this)) {
-            BaseMod.subscribe(this);
-        }
+        CardModifierManager.addModifier(this, new AimedModifier());
     }
 
     // Actions the card should do.
@@ -63,6 +59,16 @@ public class HomeSweeper extends AbstractDynamicCard implements PostEnergyRechar
         this.addToBot(new DamageAllEnemiesAction(p, this.multiDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.FIRE));
     }
 
+    @Override
+    public void calculateCardDamage(AbstractMonster mo) {
+        int temp = this.baseDamage;
+        this.baseDamage += GameActionManager.turn * magicNumber;
+        super.calculateCardDamage(mo);
+        this.baseDamage = temp;
+        this.isDamageModified = this.damage != this.baseDamage;
+
+    }
+
     // Upgraded stats.
     @Override
     public void upgrade() {
@@ -70,14 +76,6 @@ public class HomeSweeper extends AbstractDynamicCard implements PostEnergyRechar
             upgradeName();
             upgradeDamage(UPGRADE_PLUS_DMG);
             initializeDescription();
-        }
-    }
-
-    @Override
-    public void receivePostEnergyRecharge() {
-        if (!AbstractDungeon.player.masterDeck.contains(this)) {
-            upgradeDamage(magicNumber);
-            superFlash();
         }
     }
 }
