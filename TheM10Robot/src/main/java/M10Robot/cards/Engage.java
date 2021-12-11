90f91aa730deaac373c4aafcba573d12e28ee6fb
Alison Turner <alisonmoons@gmail.com>
2021-12-10 23:34:46 -0400

    Engage now also draws a card

--- TheM10Robot/src/main/java/M10Robot/cards/Engage.java
+++ TheM10Robot/src/main/java/M10Robot/cards/Engage.java
@@ -2,8 +2,10 @@ package M10Robot.cards;
 
 import M10Robot.M10RobotMod;
 import M10Robot.cards.abstractCards.AbstractDynamicCard;
+import M10Robot.cards.interfaces.ModularDescription;
 import M10Robot.characters.M10Robot;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.LockOnPower;
@@ -11,7 +13,7 @@ import com.megacrit.cardcrawl.powers.VulnerablePower;
 
 import static M10Robot.M10RobotMod.makeCardPath;
 
-public class Engage extends AbstractDynamicCard {
+public class Engage extends AbstractDynamicCard implements ModularDescription {
 
     // TEXT DECLARATION
 
@@ -36,7 +38,7 @@ public class Engage extends AbstractDynamicCard {
 
     public Engage() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
-        magicNumber = baseMagicNumber = EFFECT;
+        magicNumber = baseMagicNumber = secondMagicNumber = baseSecondMagicNumber = EFFECT;
     }
 
     // Actions the card should do.
@@ -44,6 +46,7 @@ public class Engage extends AbstractDynamicCard {
     public void use(AbstractPlayer p, AbstractMonster m) {
         this.addToBot(new ApplyPowerAction(m, p, new VulnerablePower(m, magicNumber, false)));
         this.addToBot(new ApplyPowerAction(m, p, new LockOnPower(m, magicNumber)));
+        this.addToBot(new DrawCardAction(secondMagicNumber));
     }
 
     //Upgraded stats.
@@ -53,6 +56,17 @@ public class Engage extends AbstractDynamicCard {
             upgradeName();
             upgradeMagicNumber(UPGRADE_PLUS_EFFECT);
             initializeDescription();
+        }
+    }
+
+    @Override
+    public void changeDescription() {
+        if (DESCRIPTION != null) {
+            if (secondMagicNumber > 1) {
+                rawDescription = UPGRADE_DESCRIPTION;
+            } else {
+                rawDescription = DESCRIPTION;
+            }
         }
     }
 }
