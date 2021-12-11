a9c04fc18228923ad5f2d2a018e951bd9205b914
Alison Turner <alisonmoons@gmail.com>
2021-12-11 00:25:38 -0400

    Added Speed Loader, draws a card when you Reload

---
+++ TheM10Robot/src/main/java/M10Robot/cards/Speedloader.java
@@ -0,0 +1,76 @@
+package M10Robot.cards;
+
+import M10Robot.M10RobotMod;
+import M10Robot.cards.abstractCards.AbstractDynamicCard;
+import M10Robot.cards.interfaces.ModularDescription;
+import M10Robot.characters.M10Robot;
+import M10Robot.powers.SpeedloaderPower;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static M10Robot.M10RobotMod.makeCardPath;
+
+public class Speedloader extends AbstractDynamicCard implements ModularDescription {
+
+    /*
+     * Wiki-page: https://github.com/daviscook477/BaseMod/wiki/Custom-Cards
+     *
+     * Defend Gain 5 (8) block.
+     */
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = M10RobotMod.makeID(Speedloader.class.getSimpleName());
+    public static final String IMG = makeCardPath("Speedloader2.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.NONE;
+    private static final CardType TYPE = CardType.POWER;
+    public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int UPGRADE_COST = 0;
+    private static final int EFFECT = 1;
+
+    // /STAT DECLARATION/
+
+
+    public Speedloader() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        magicNumber = baseMagicNumber = EFFECT;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new ApplyPowerAction(p, p, new SpeedloaderPower(p, magicNumber)));
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBaseCost(UPGRADE_COST);
+            initializeDescription();
+        }
+    }
+
+    @Override
+    public void changeDescription() {
+        if (DESCRIPTION != null) {
+            if (magicNumber > 1) {
+                rawDescription = UPGRADE_DESCRIPTION;
+            } else {
+                rawDescription = DESCRIPTION;
+            }
+        }
+    }
+}
