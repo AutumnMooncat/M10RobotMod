2c03574778032696d507528bc530d766687ff604
Alison Turner <alisonmoons@gmail.com>
2021-12-10 23:39:45 -0400

    Spectral Cannon now uses new Reloading mechanics

--- TheM10Robot/src/main/java/M10Robot/cards/SpectralCannon.java
+++ TheM10Robot/src/main/java/M10Robot/cards/SpectralCannon.java
@@ -5,17 +5,13 @@ import M10Robot.cards.abstractCards.AbstractReloadableCard;
 import M10Robot.characters.M10Robot;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.actions.utility.SFXAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.AbstractPower;
-import com.megacrit.cardcrawl.powers.WeakPower;
 import com.megacrit.cardcrawl.vfx.combat.ExplosionSmallEffect;
-import com.megacrit.cardcrawl.vfx.combat.SmallLaserEffect;
 
 import static M10Robot.M10RobotMod.makeCardPath;
 
@@ -46,14 +42,12 @@ public class SpectralCannon extends AbstractReloadableCard {
     private static final int COST = 1;
     private static final int DAMAGE = 10;
     private static final int UPGRADE_PLUS_DMG = 3;
-    private static final int SHOTS = 1;
 
     // /STAT DECLARATION/
 
     public SpectralCannon() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         baseDamage = damage = DAMAGE;
-        ammoCount = baseAmmoCount = thirdMagicNumber = baseThirdMagicNumber = SHOTS;
         isMultiDamage = true;
         isEthereal = true;
     }
