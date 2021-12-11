70baad1c442bd9b42646ee77218061058ef921bc
Alison Turner <alisonmoons@gmail.com>
2021-12-11 00:22:11 -0400

    Added IntensifyAction

---
+++ TheM10Robot/src/main/java/M10Robot/actions/IntensifyAction.java
@@ -0,0 +1,59 @@
+package M10Robot.actions;
+
+import M10Robot.cards.interfaces.ModularDescription;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class IntensifyAction extends AbstractGameAction {
+    public enum EffectType {
+        DAMAGE,
+        BLOCK,
+        MAGIC
+    }
+    AbstractCard card;
+    EffectType type;
+
+    public IntensifyAction(AbstractCard card, int amount, EffectType type) {
+        this.amount = amount;
+        this.card = card;
+        this.type = type;
+    }
+
+    @Override
+    public void update() {
+        ArrayList<AbstractCard> matches = new ArrayList<>();
+        matches.add(card);
+        matches.addAll(AbstractDungeon.player.hand.group.stream().filter(c -> c.getClass().equals(card.getClass())).collect(Collectors.toCollection(ArrayList::new)));
+        matches.addAll(AbstractDungeon.player.drawPile.group.stream().filter(c -> c.getClass().equals(card.getClass())).collect(Collectors.toCollection(ArrayList::new)));
+        matches.addAll(AbstractDungeon.player.discardPile.group.stream().filter(c -> c.getClass().equals(card.getClass())).collect(Collectors.toCollection(ArrayList::new)));
+
+        for (AbstractCard c : matches) {
+            applyVarChange(c);
+        }
+
+        this.isDone = true;
+    }
+
+    private void applyVarChange(AbstractCard c) {
+        switch (type) {
+            case DAMAGE:
+                c.baseDamage += amount;
+                break;
+            case BLOCK:
+                c.baseBlock += amount;
+                break;
+            case MAGIC:
+                c.baseMagicNumber += amount;
+                c.magicNumber += amount;
+                break;
+        }
+        c.applyPowers();
+        if (c instanceof ModularDescription) {
+            c.initializeDescription();
+        }
+    }
+}
