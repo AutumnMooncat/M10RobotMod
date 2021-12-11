afd4fb7ab52b174828657967c6023f4c9f41ec11
Alison Turner <alisonmoons@gmail.com>
2021-12-10 23:30:13 -0400

    Divided Attention now gives Lock-On and card draw

--- TheM10Robot/src/main/java/M10Robot/cards/DividedAttention.java
+++ TheM10Robot/src/main/java/M10Robot/cards/DividedAttention.java
@@ -6,12 +6,10 @@ import M10Robot.cards.interfaces.ModularDescription;
 import M10Robot.characters.M10Robot;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
-import com.megacrit.cardcrawl.actions.common.GainBlockAction;
-import com.megacrit.cardcrawl.actions.defect.IncreaseMaxOrbAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.FocusPower;
-import com.megacrit.cardcrawl.powers.StrengthPower;
+import com.megacrit.cardcrawl.powers.LockOnPower;
 
 import static M10Robot.M10RobotMod.makeCardPath;
 
@@ -28,13 +26,14 @@ public class DividedAttention extends AbstractDynamicCard implements ModularDesc
     // STAT DECLARATION
 
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
-    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardTarget TARGET = CardTarget.ALL;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
 
     private static final int COST = 1;
-    private static final int EFFECT = 1;
+    private static final int EFFECT = 2;
     private static final int UPGRADE_PLUS_EFFECT = 1;
+    private static final int DRAW = 1;
 
     // /STAT DECLARATION/
 
@@ -42,14 +41,18 @@ public class DividedAttention extends AbstractDynamicCard implements ModularDesc
     public DividedAttention() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         magicNumber = baseMagicNumber = EFFECT;
+        secondMagicNumber = baseSecondMagicNumber = DRAW;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new ApplyPowerAction(p, p, new FocusPower(p, -magicNumber)));
-        this.addToBot(new ApplyPowerAction(p, p, new StrengthPower(p, magicNumber)));
-        this.addToBot(new IncreaseMaxOrbAction(magicNumber));
+        for (AbstractMonster aM : AbstractDungeon.getMonsters().monsters) {
+            if (!aM.isDeadOrEscaped()) {
+                this.addToBot(new ApplyPowerAction(aM, p, new LockOnPower(aM, magicNumber), magicNumber, true));
+            }
+        }
+        this.addToBot(new DrawCardAction(secondMagicNumber));
     }
 
     //Upgraded stats.
@@ -65,7 +68,7 @@ public class DividedAttention extends AbstractDynamicCard implements ModularDesc
     @Override
     public void changeDescription() {
         if (DESCRIPTION != null) {
-            if (magicNumber > 1) {
+            if (secondMagicNumber > 1) {
                 rawDescription = UPGRADE_DESCRIPTION;
             } else {
                 rawDescription = DESCRIPTION;
