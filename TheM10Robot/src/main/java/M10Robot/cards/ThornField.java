91c8ba68d41fff6a2abee8c53ce8c5b1e7cfe733
Alison Turner <alisonmoons@gmail.com>
2021-12-11 00:31:48 -0400

    Added Thorn Field, Deals damage to ALL enemies, gives Spikes, and is Spiky

---
+++ TheM10Robot/src/main/java/M10Robot/cards/ThornField.java
@@ -0,0 +1,69 @@
+package M10Robot.cards;
+
+import M10Robot.M10RobotMod;
+import M10Robot.cardModifiers.SpikyModifier;
+import M10Robot.cards.abstractCards.AbstractDynamicCard;
+import M10Robot.characters.M10Robot;
+import M10Robot.powers.SpikesPower;
+import basemod.helpers.CardModifierManager;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static M10Robot.M10RobotMod.makeCardPath;
+
+public class ThornField extends AbstractDynamicCard {
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = M10RobotMod.makeID(ThornField.class.getSimpleName());
+    public static final String IMG = makeCardPath("ThornField.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.ALL;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int DAMAGE = 5;
+    private static final int UPGRADE_PLUS_DMG = 3;
+    private static final int SPIKES = 2;
+    private static final int UPGRADE_PLUS_SPIKES = 1;
+
+    // /STAT DECLARATION/
+
+    public ThornField() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = damage = DAMAGE;
+        magicNumber = baseMagicNumber = SPIKES;
+        isMultiDamage = true;
+        CardModifierManager.addModifier(this, new SpikyModifier());
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        //this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
+        this.addToBot(new DamageAllEnemiesAction(p, multiDamage, damageTypeForTurn, AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
+        this.addToBot(new ApplyPowerAction(p, p, new SpikesPower(p, magicNumber)));
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            upgradeMagicNumber(UPGRADE_PLUS_SPIKES);
+            initializeDescription();
+        }
+    }
+}
