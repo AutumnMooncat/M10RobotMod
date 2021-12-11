d99fb15b67cd323001041801ba95064de04b77a8
Alison Turner <alisonmoons@gmail.com>
2021-12-11 00:17:00 -0400

    Juggling now draws 2 cards and draws an extra card next turn

--- TheM10Robot/src/main/java/M10Robot/cards/Juggling.java
+++ TheM10Robot/src/main/java/M10Robot/cards/Juggling.java
@@ -8,12 +8,15 @@ import M10Robot.orbs.BombOrb;
 import M10Robot.orbs.PresentOrb;
 import M10Robot.orbs.SearchlightOrb;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.actions.defect.ChannelAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.orbs.*;
+import com.megacrit.cardcrawl.powers.DrawCardNextTurnPower;
 import com.megacrit.cardcrawl.powers.FocusPower;
+import com.megacrit.cardcrawl.powers.NextTurnBlockPower;
 
 import java.util.ArrayList;
 
@@ -37,8 +40,8 @@ public class Juggling extends AbstractDynamicCard {
     public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
 
     private static final int COST = 1;
-    private static final int ORBS = 2;
-    private static final int UPGRADE_PLUS_ORBS = 1;
+    private static final int CARDS = 2;
+    private static final int UPGRADE_PLUS_CARDS = 1;
     private static final int FOCUS_LOST = 1;
 
     // /STAT DECLARATION/
@@ -46,22 +49,14 @@ public class Juggling extends AbstractDynamicCard {
 
     public Juggling() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
-        magicNumber = baseMagicNumber = ORBS;
-        secondMagicNumber = baseSecondMagicNumber = FOCUS_LOST;
+        magicNumber = baseMagicNumber = CARDS;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        for (int i = 0 ; i < magicNumber ; i++) {
-            ArrayList<AbstractOrb> orbs = new ArrayList<>();
-            orbs.add(new SearchlightOrb());
-            orbs.add(new BitOrb());
-            orbs.add(new BombOrb());
-            orbs.add(new PresentOrb());
-            this.addToBot(new ChannelAction(orbs.get(AbstractDungeon.cardRandomRng.random(orbs.size() - 1))));
-        }
-        this.addToBot(new ApplyPowerAction(p, p, new FocusPower(p, -secondMagicNumber)));
+        this.addToBot(new DrawCardAction(magicNumber));
+        this.addToBot(new ApplyPowerAction(p, p, new DrawCardNextTurnPower(p, 1)));
     }
 
     //Upgraded stats.
@@ -69,7 +64,7 @@ public class Juggling extends AbstractDynamicCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeMagicNumber(UPGRADE_PLUS_ORBS);
+            upgradeMagicNumber(UPGRADE_PLUS_CARDS);
             initializeDescription();
         }
     }
