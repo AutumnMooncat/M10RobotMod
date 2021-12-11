3223f1a55de7c3721e2e225d22d8fc5dde9b7a45
Alison Turner <alisonmoons@gmail.com>
2021-12-11 00:19:36 -0400

    RAM Upgrade reimplemented as a Power

---
+++ TheM10Robot/src/main/java/M10Robot/powers/RAMUpgradePower.java
@@ -0,0 +1,67 @@
+package M10Robot.powers;
+
+import M10Robot.M10RobotMod;
+import M10Robot.orbs.AbstractCustomOrb;
+import basemod.interfaces.CloneablePowerInterface;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.orbs.AbstractOrb;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+public class RAMUpgradePower extends AbstractPower implements CloneablePowerInterface {
+
+    public static final String POWER_ID = M10RobotMod.makeID("RAMUpgradePower");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
+    // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
+    //private static final Texture tex84 = TextureLoader.getTexture(makePowerPath("placeholder_power84.png"));
+    //private static final Texture tex32 = TextureLoader.getTexture(makePowerPath("placeholder_power32.png"));
+
+    public RAMUpgradePower(AbstractCreature owner, int amount) {
+        this.name = NAME;
+        this.ID = POWER_ID;
+        this.owner = owner;
+        this.amount = amount;
+
+        this.type = PowerType.BUFF;
+        this.isTurnBased = false;
+
+        // We load those txtures here.
+        //this.loadRegion("cExplosion");
+        this.loadRegion("ai");
+        //this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        //this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+
+        updateDescription();
+    }
+
+    public void updateDescription() {
+        if (amount == 1) {
+            this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[1];
+        } else {
+            this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[2];
+        }
+    }
+
+    @Override
+    public void onChannel(AbstractOrb orb) {
+        if (orb instanceof AbstractCustomOrb) {
+            flash();
+            for (int i = 0 ; i < amount ; i++) {
+                if (((AbstractCustomOrb) orb).canUpgrade()) {
+                    ((AbstractCustomOrb) orb).upgrade();
+                }
+            }
+        }
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new RAMUpgradePower(owner, amount);
+    }
+
+}
