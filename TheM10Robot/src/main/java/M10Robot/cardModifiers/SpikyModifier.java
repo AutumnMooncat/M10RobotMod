41ead8c7ae5602bf9eec8946eb9caec61de0462c
Alison Turner <alisonmoons@gmail.com>
2021-12-10 23:16:56 -0400

    Added Spikes (Thorn but decays), and Spiky Modifier (damage scales with Spikes)

---
+++ TheM10Robot/src/main/java/M10Robot/cardModifiers/SpikyModifier.java
@@ -0,0 +1,42 @@
+package M10Robot.cardModifiers;
+
+import M10Robot.M10RobotMod;
+import M10Robot.powers.SpikesPower;
+import basemod.abstracts.AbstractCardModifier;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.LockOnPower;
+
+public class SpikyModifier extends AbstractCardModifier {
+    public static final String ID = M10RobotMod.makeID("SpikyModifier");
+    public static final CardStrings STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+    private static final String[] TEXT = STRINGS.EXTENDED_DESCRIPTION;
+
+    @Override
+    public float modifyDamage(float damage, DamageInfo.DamageType type, AbstractCard card, AbstractMonster target) {
+        if (AbstractDungeon.player.hasPower(SpikesPower.POWER_ID)) {
+            damage += AbstractDungeon.player.getPower(SpikesPower.POWER_ID).amount;
+        }
+        return damage;
+    }
+
+    @Override
+    public String modifyDescription(String rawDescription, AbstractCard card) {
+        rawDescription = TEXT[0] + rawDescription;
+        return rawDescription;
+    }
+
+    @Override
+    public AbstractCardModifier makeCopy() {
+        return new AimedModifier();
+    }
+
+    @Override
+    public boolean isInherent(AbstractCard card) {
+        return true;
+    }
+}
---
+++ TheM10Robot/src/main/java/M10Robot/powers/SpikesPower.java
@@ -0,0 +1,64 @@
+package M10Robot.powers;
+
+import M10Robot.M10RobotMod;
+import basemod.interfaces.CloneablePowerInterface;
+import com.evacipated.cardcrawl.mod.stslib.patches.NeutralPowertypePatch;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+public class SpikesPower extends AbstractPower implements CloneablePowerInterface {
+
+    public static final String POWER_ID = M10RobotMod.makeID("SpikesPower");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
+    // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
+    //private static final Texture tex84 = TextureLoader.getTexture(makePowerPath("placeholder_power84.png"));
+    //private static final Texture tex32 = TextureLoader.getTexture(makePowerPath("placeholder_power32.png"));
+
+    public SpikesPower(AbstractCreature owner, int amount) {
+        this.name = NAME;
+        this.ID = POWER_ID;
+        this.owner = owner;
+        this.amount = amount;
+
+        this.type = PowerType.BUFF;
+        this.isTurnBased = false;
+
+        // We load those txtures here.
+        //this.loadRegion("cExplosion");
+        this.loadRegion("thorns");
+        //this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        //this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+
+        updateDescription();
+    }
+
+    public int onAttacked(DamageInfo info, int damageAmount) {
+        if (info.type != DamageInfo.DamageType.THORNS && info.type != DamageInfo.DamageType.HP_LOSS && info.owner != null && info.owner != this.owner) {
+            this.flash();
+            this.addToTop(new ReducePowerAction(owner, owner, this, 1));
+            this.addToTop(new DamageAction(info.owner, new DamageInfo(this.owner, this.amount, DamageInfo.DamageType.THORNS), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL, true));
+        }
+
+        return damageAmount;
+    }
+
+    public void updateDescription() {
+        this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[1];
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new SpikesPower(owner, amount);
+    }
+
+}
