a5dd69a86c84c69cc5b727cbf075884f952bc16d
Alison Turner <alisonmoons@gmail.com>
2021-12-10 23:39:22 -0400

    Split beam now deals damage to a single target and then all enemies. Old effect wrapped into unused SplitBeamAction

---
+++ TheM10Robot/src/main/java/M10Robot/actions/SplitBeamAction.java
@@ -0,0 +1,56 @@
+package M10Robot.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.utility.SFXAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.ExplosionSmallEffect;
+import com.megacrit.cardcrawl.vfx.combat.SmallLaserEffect;
+
+import java.util.HashMap;
+
+public class SplitBeamAction extends AbstractGameAction {
+    DamageInfo.DamageType type;
+    HashMap<AbstractMonster, Integer> hitMap;
+    int[] damage;
+
+    public SplitBeamAction(AbstractCreature source, int[] damage, DamageInfo.DamageType damageType, int hits) {
+        this.source = source;
+        this.amount = hits;
+        this.type = damageType;
+        this.damage = damage;
+        this.hitMap = new HashMap<>();
+    }
+
+    private SplitBeamAction(AbstractCreature source, int[] damage, DamageInfo.DamageType damageType, int hits, HashMap<AbstractMonster, Integer> hitMap) {
+        this.source = source;
+        this.amount = hits;
+        this.type = damageType;
+        this.damage = damage;
+        this.hitMap = hitMap;
+    }
+
+    @Override
+    public void update() {
+        if (amount > 0) {
+            AbstractMonster mo = AbstractDungeon.getRandomMonster();
+            hitMap.put(mo, hitMap.getOrDefault(mo, 0)+1);
+            this.addToTop(new SplitBeamAction(source, damage, damageType, amount-1, hitMap));
+            this.addToTop(new DamageAction(mo, new DamageInfo(source, damage[AbstractDungeon.getMonsters().monsters.indexOf(mo)], type), AbstractGameAction.AttackEffect.NONE, true));
+            this.addToTop(new VFXAction(new SmallLaserEffect(mo.hb.cX, mo.hb.cY, source.hb.cX, source.hb.cY), 0.1F));
+            this.addToTop(new SFXAction("ATTACK_MAGIC_BEAM_SHORT", 0.5F));
+        } else {
+            for (AbstractMonster mo : hitMap.keySet()) {
+                if (hitMap.get(mo) > 1 && !mo.isDeadOrEscaped()) {
+                    this.addToTop(new DamageAction(mo, new DamageInfo(source, damage[AbstractDungeon.getMonsters().monsters.indexOf(mo)], type), AbstractGameAction.AttackEffect.NONE, true));
+                    this.addToTop(new VFXAction(new ExplosionSmallEffect(mo.hb.cX, mo.hb.cY), 0.1F));
+                }
+            }
+        }
+        this.isDone = true;
+    }
+}
--- TheM10Robot/src/main/java/M10Robot/cards/SplitBeam.java
+++ TheM10Robot/src/main/java/M10Robot/cards/SplitBeam.java
@@ -1,22 +1,19 @@
 package M10Robot.cards;
 
 import M10Robot.M10RobotMod;
+import M10Robot.cardModifiers.AimedModifier;
 import M10Robot.cards.abstractCards.AbstractDynamicCard;
 import M10Robot.characters.M10Robot;
+import basemod.helpers.CardModifierManager;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.utility.SFXAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.WeakPower;
-import com.megacrit.cardcrawl.vfx.combat.ExplosionSmallEffect;
 import com.megacrit.cardcrawl.vfx.combat.SmallLaserEffect;
-
-import java.util.HashMap;
 
 import static M10Robot.M10RobotMod.makeCardPath;
 
@@ -34,39 +31,34 @@ public class SplitBeam extends AbstractDynamicCard {
     // STAT DECLARATION
 
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
-    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
 
     private static final int COST = 1;
     private static final int DAMAGE = 4;
     private static final int UPGRADE_PLUS_DMG = 2;
-    private static final int HITS = 2;
-    private static final int UPGRADE_PLUS_HITS = 1;
+    private static final int HITS = 4;
+    private static final int UPGRADE_PLUS_HITS = 2;
 
     // /STAT DECLARATION/
 
     public SplitBeam() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         baseDamage = damage = DAMAGE;
-        magicNumber = baseMagicNumber = HITS;
+        CardModifierManager.addModifier(this, new AimedModifier());
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        HashMap<AbstractMonster, Integer> hitMap = new HashMap<>();
-        for (int i = 0; i < magicNumber ; i++) {
-            AbstractMonster mo = AbstractDungeon.getRandomMonster();
-            hitMap.put(mo, hitMap.getOrDefault(mo, 0)+1);
-            this.addToBot(new SFXAction("ATTACK_MAGIC_BEAM_SHORT", 0.5F));
-            this.addToBot(new VFXAction(new SmallLaserEffect(mo.hb.cX, mo.hb.cY, p.hb.cX, p.hb.cY), 0.1F));
-            this.addToBot(new DamageAction(mo, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.NONE, true));
-        }
-
-        for (AbstractMonster mo : hitMap.keySet()) {
-            if (hitMap.get(mo) > 1) {
-                this.addToBot(new VFXAction(new ExplosionSmallEffect(mo.hb.cX, mo.hb.cY), 0.1F));
+        this.addToBot(new SFXAction("ATTACK_MAGIC_BEAM_SHORT", 0.5F));
+        this.addToBot(new VFXAction(new SmallLaserEffect(m.hb.cX, m.hb.cY, p.hb.cX, p.hb.cY), 0.1F));
+        this.addToBot(new DamageAction(m, new DamageInfo(p, damage)));
+        for (AbstractMonster mo : AbstractDungeon.getMonsters().monsters) {
+            if (!mo.isDeadOrEscaped()) {
+                this.addToBot(new SFXAction("ATTACK_MAGIC_BEAM_SHORT", 0.5F));
+                this.addToBot(new VFXAction(new SmallLaserEffect(mo.hb.cX, mo.hb.cY, p.hb.cX, p.hb.cY), 0.0F));
                 this.addToBot(new DamageAction(mo, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.NONE, true));
             }
         }
@@ -77,8 +69,7 @@ public class SplitBeam extends AbstractDynamicCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            //upgradeDamage(UPGRADE_PLUS_DMG);
-            upgradeMagicNumber(UPGRADE_PLUS_HITS);
+            upgradeDamage(UPGRADE_PLUS_DMG);
             initializeDescription();
         }
     }
