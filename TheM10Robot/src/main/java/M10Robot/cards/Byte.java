6f45e29497af282744e7f84c14c0b3992420997b
Alison Turner <alisonmoons@gmail.com>
2021-12-13 11:53:35 -0400

    Added Byte, Loaded attack that channels 2 Bit

---
+++ TheM10Robot/src/main/java/M10Robot/cards/Byte.java
@@ -0,0 +1,68 @@
+package M10Robot.cards;
+
+import M10Robot.M10RobotMod;
+import M10Robot.actions.MultichannelAction;
+import M10Robot.cardModifiers.AimedModifier;
+import M10Robot.cards.abstractCards.AbstractDynamicCard;
+import M10Robot.cards.abstractCards.AbstractReloadableCard;
+import M10Robot.characters.M10Robot;
+import M10Robot.orbs.BitOrb;
+import basemod.helpers.CardModifierManager;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+
+import static M10Robot.M10RobotMod.makeCardPath;
+
+public class Byte extends AbstractReloadableCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = M10RobotMod.makeID(Byte.class.getSimpleName());
+    public static final String IMG = makeCardPath("Byte.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
+
+    private static final int COST = 2;
+    private static final int DAMAGE = 12;
+    private static final int UPGRADE_PLUS_DMG = 4;
+    private static final int ORBS = 2;
+
+    // /STAT DECLARATION/
+
+    public Byte() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = damage = DAMAGE;
+        magicNumber = baseMagicNumber = ORBS;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE));
+        this.addToBot(new ApplyPowerAction(m, p, new VulnerablePower(m, magicNumber, false)));
+        this.addToBot(new MultichannelAction(new BitOrb(), magicNumber));
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            initializeDescription();
+        }
+    }
+}
