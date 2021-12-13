dd4fc3d7a019eefa3ebc8975f6319898733583bd
Alison Turner <alisonmoons@gmail.com>
2021-12-13 11:54:22 -0400

    Quick Thinking no channels a Bit but is 0 cost

--- TheM10Robot/src/main/java/M10Robot/cards/QuickThinking.java
+++ TheM10Robot/src/main/java/M10Robot/cards/QuickThinking.java
@@ -30,7 +30,7 @@ public class QuickThinking extends AbstractDynamicCard implements ModularDescrip
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
 
-    private static final int COST = 1;
+    private static final int COST = 0;
     private static final int DRAW = 1;
     private static final int UPGRADE_PLUS_DRAW = 1;
 
@@ -40,14 +40,12 @@ public class QuickThinking extends AbstractDynamicCard implements ModularDescrip
     public QuickThinking() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         magicNumber = baseMagicNumber = DRAW;
-        secondMagicNumber = baseSecondMagicNumber = DRAW;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         this.addToBot(new DrawCardAction(magicNumber, new QuickThinkingAction()));
-        this.addToBot(new MultichannelAction(new BitOrb(), secondMagicNumber));
     }
 
     //Upgraded stats.
