96a68ae63d297dcb4bb3e77cd6ce3a3dbc4bd1fd
Alison Turner <alisonmoons@gmail.com>
2021-12-13 11:54:55 -0400

    Salvo now a 1 cost Uncommon with higher damage

--- TheM10Robot/src/main/java/M10Robot/cards/Salvo.java
+++ TheM10Robot/src/main/java/M10Robot/cards/Salvo.java
@@ -24,13 +24,13 @@ public class Salvo extends AbstractDynamicCard {
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
 
     private static final int COST = 1;
-    private static final int DAMAGE = 4;
+    private static final int DAMAGE = 6;
     private static final int UPGRADE_PLUS_DMG = 2;
 
     // /STAT DECLARATION/
