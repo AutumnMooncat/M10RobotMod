41f4ec05ecbd694a358e372ab863872fa283dc71
Alison Turner <alisonmoons@gmail.com>
2021-12-14 10:25:07 -0400

    Health Down effect increased from 6(10) to 8(12)

--- TheM10Robot/src/main/java/M10Robot/cards/HealthDown.java
+++ TheM10Robot/src/main/java/M10Robot/cards/HealthDown.java
@@ -31,7 +31,7 @@ public class HealthDown extends AbstractDynamicCard {
     public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
 
     private static final int COST = 1;
-    private static final int EFFECT = 6;
+    private static final int EFFECT = 8;
     private static final int UPGRADE_PLUS_EFFECT = 4;
 
     // /STAT DECLARATION/
