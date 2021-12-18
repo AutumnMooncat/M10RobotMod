069e0f9a44331b80b1c026f2abd1c0272a5d540f
Alison Turner <alisonmoons@gmail.com>
2021-12-18 16:38:08 -0400

    Removed unused imports

--- TheM10Robot/src/main/java/M10Robot/cards/Strike.java
+++ TheM10Robot/src/main/java/M10Robot/cards/Strike.java
@@ -7,8 +7,6 @@ import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static M10Robot.M10RobotMod.makeCardPath;
