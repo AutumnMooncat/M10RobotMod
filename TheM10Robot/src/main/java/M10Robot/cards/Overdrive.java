5a44b34dec6afb1166905b78b5801ef157bc7bf6
Alison Turner <alisonmoons@gmail.com>
2021-12-19 17:40:52 -0400

    Removed Spiker Form in favor or Overdrive Form

---
+++ TheM10Robot/src/main/java/M10Robot/cards/Overdrive.java
@@ -0,0 +1,60 @@
+package M10Robot.cards;
+
+import M10Robot.M10RobotMod;
+import M10Robot.cards.abstractCards.AbstractDynamicCard;
+import M10Robot.characters.M10Robot;
+import M10Robot.powers.OverdrivePower;
+import basemod.helpers.BaseModCardTags;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static M10Robot.M10RobotMod.makeCardPath;
+
+public class Overdrive extends AbstractDynamicCard {
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = M10RobotMod.makeID(Overdrive.class.getSimpleName());
+    public static final String IMG = makeCardPath("Overdrive.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.NONE;
+    private static final CardType TYPE = CardType.POWER;
+    public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
+
+    private static final int COST = 3;
+    private static final int AMOUNT = 2;
+    private static final int UPGRADE_PLUS_AMOUNT = 1;
+
+    // /STAT DECLARATION/
+
+
+    public Overdrive() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        magicNumber = baseMagicNumber = AMOUNT;
+        this.tags.add(BaseModCardTags.FORM);
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new ApplyPowerAction(p, p, new OverdrivePower(p, magicNumber)));
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_PLUS_AMOUNT);
+            initializeDescription();
+        }
+    }
+}
