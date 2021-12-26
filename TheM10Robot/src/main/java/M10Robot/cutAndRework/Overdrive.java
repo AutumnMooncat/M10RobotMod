2c6e7cdf133a7defefb882dce994629b79a9d019
Alison Turner <alisonmoons@gmail.com>
2021-12-26 19:51:43 -0400

    Removed a few cards that need reworks

--- TheM10Robot/src/main/java/M10Robot/cutAndRework/Overdrive.java
+++ TheM10Robot/src/main/java/M10Robot/cutAndRework/Overdrive.java
@@ -1,55 +1,51 @@
-package M10Robot.cards;
+package M10Robot.cutAndRework;
 
 import M10Robot.M10RobotMod;
 import M10Robot.cards.abstractCards.AbstractDynamicCard;
 import M10Robot.characters.M10Robot;
-import M10Robot.powers.SpeedloaderPower;
+import M10Robot.powers.OverdrivePower;
+import basemod.helpers.BaseModCardTags;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static M10Robot.M10RobotMod.makeCardPath;
 
-public class Speedloader extends AbstractDynamicCard {
-
-    /*
-     * Wiki-page: https://github.com/daviscook477/BaseMod/wiki/Custom-Cards
-     *
-     * Defend Gain 5 (8) block.
-     */
+public class Overdrive extends AbstractDynamicCard {
 
 
     // TEXT DECLARATION
 
-    public static final String ID = M10RobotMod.makeID(Speedloader.class.getSimpleName());
-    public static final String IMG = makeCardPath("Speedloader2.png");
+    public static final String ID = M10RobotMod.makeID(Overdrive.class.getSimpleName());
+    public static final String IMG = makeCardPath("Overdrive.png");
 
     // /TEXT DECLARATION/
 
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardRarity RARITY = CardRarity.RARE;
     private static final CardTarget TARGET = CardTarget.NONE;
     private static final CardType TYPE = CardType.POWER;
     public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
 
-    private static final int COST = 1;
-    private static final int UPGRADE_COST = 0;
-    private static final int EFFECT = 1;
+    private static final int COST = 3;
+    private static final int AMOUNT = 50;
+    private static final int UPGRADE_PLUS_AMOUNT = 25;
 
     // /STAT DECLARATION/
 
 
-    public Speedloader() {
+    public Overdrive() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
-        magicNumber = baseMagicNumber = EFFECT;
+        magicNumber = baseMagicNumber = AMOUNT;
+        this.tags.add(BaseModCardTags.FORM);
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new ApplyPowerAction(p, p, new SpeedloaderPower(p, magicNumber)));
+        this.addToBot(new ApplyPowerAction(p, p, new OverdrivePower(p, magicNumber)));
     }
 
     //Upgraded stats.
@@ -57,7 +53,7 @@ public class Speedloader extends AbstractDynamicCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeBaseCost(UPGRADE_COST);
+            upgradeMagicNumber(UPGRADE_PLUS_AMOUNT);
             initializeDescription();
         }
     }
--- TheM10Robot/src/main/java/M10Robot/cutAndRework/Speedloader.java
+++ TheM10Robot/src/main/java/M10Robot/cutAndRework/Speedloader.java
@@ -1,51 +1,55 @@
-package M10Robot.cards;
+package M10Robot.cutAndRework;
 
 import M10Robot.M10RobotMod;
 import M10Robot.cards.abstractCards.AbstractDynamicCard;
 import M10Robot.characters.M10Robot;
-import M10Robot.powers.OverdrivePower;
-import basemod.helpers.BaseModCardTags;
+import M10Robot.powers.SpeedloaderPower;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static M10Robot.M10RobotMod.makeCardPath;
 
-public class Overdrive extends AbstractDynamicCard {
+public class Speedloader extends AbstractDynamicCard {
+
+    /*
+     * Wiki-page: https://github.com/daviscook477/BaseMod/wiki/Custom-Cards
+     *
+     * Defend Gain 5 (8) block.
+     */
 
 
     // TEXT DECLARATION
 
-    public static final String ID = M10RobotMod.makeID(Overdrive.class.getSimpleName());
-    public static final String IMG = makeCardPath("Overdrive.png");
+    public static final String ID = M10RobotMod.makeID(Speedloader.class.getSimpleName());
+    public static final String IMG = makeCardPath("Speedloader2.png");
 
     // /TEXT DECLARATION/
 
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.NONE;
     private static final CardType TYPE = CardType.POWER;
     public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
 
-    private static final int COST = 3;
-    private static final int AMOUNT = 50;
-    private static final int UPGRADE_PLUS_AMOUNT = 25;
+    private static final int COST = 1;
+    private static final int UPGRADE_COST = 0;
+    private static final int EFFECT = 1;
 
     // /STAT DECLARATION/
 
 
-    public Overdrive() {
+    public Speedloader() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
-        magicNumber = baseMagicNumber = AMOUNT;
-        this.tags.add(BaseModCardTags.FORM);
+        magicNumber = baseMagicNumber = EFFECT;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new ApplyPowerAction(p, p, new OverdrivePower(p, magicNumber)));
+        this.addToBot(new ApplyPowerAction(p, p, new SpeedloaderPower(p, magicNumber)));
     }
 
     //Upgraded stats.
@@ -53,7 +57,7 @@ public class Overdrive extends AbstractDynamicCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeMagicNumber(UPGRADE_PLUS_AMOUNT);
+            upgradeBaseCost(UPGRADE_COST);
             initializeDescription();
         }
     }
