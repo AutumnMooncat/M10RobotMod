0d4dc693a6af3b126251481334b9d67229031dfb
Alison Turner <alisonmoons@gmail.com>
2021-12-30 14:15:33 -0400

    Added Booster Shot, deals damage and upgrades all orbs

---
+++ TheM10Robot/src/main/java/M10Robot/cards/BoosterShot.java
@@ -0,0 +1,66 @@
+package M10Robot.cards;
+
+import M10Robot.M10RobotMod;
+import M10Robot.actions.UpgradeOrbsAction;
+import M10Robot.cardModifiers.AimedModifier;
+import M10Robot.cards.abstractCards.AbstractDynamicCard;
+import M10Robot.characters.M10Robot;
+import basemod.helpers.CardModifierManager;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static M10Robot.M10RobotMod.makeCardPath;
+
+public class BoosterShot extends AbstractDynamicCard {
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = M10RobotMod.makeID(BoosterShot.class.getSimpleName());
+    public static final String IMG = makeCardPath("BoosterShot2.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final AbstractCard.CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final AbstractCard.CardTarget TARGET = CardTarget.ENEMY;
+    private static final AbstractCard.CardType TYPE = CardType.ATTACK;
+    public static final AbstractCard.CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
+
+    private static final int COST = 2;
+    private static final int DAMAGE = 10;
+    private static final int UPGRADE_PLUS_DMG = 4;
+    private static final int UPGRADES = 2;
+
+    // /STAT DECLARATION/
+
+    public BoosterShot() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = damage = DAMAGE;
+        magicNumber = baseMagicNumber = UPGRADES;
+        CardModifierManager.addModifier(this, new AimedModifier());
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
+        this.addToBot(new UpgradeOrbsAction(magicNumber));
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            initializeDescription();
+        }
+    }
+}
