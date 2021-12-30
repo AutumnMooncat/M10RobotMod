a73da619d82771ed9684c4ecd0758908c32876da
Alison Turner <alisonmoons@gmail.com>
2021-12-30 14:15:09 -0400

    Added Quick Boot, Innate card that channels 3 Orbs.

---
+++ TheM10Robot/src/main/java/M10Robot/cards/QuickBoot.java
@@ -0,0 +1,73 @@
+package M10Robot.cards;
+
+import M10Robot.M10RobotMod;
+import M10Robot.cards.abstractCards.AbstractDynamicCard;
+import M10Robot.characters.M10Robot;
+import M10Robot.orbs.BitOrb;
+import M10Robot.orbs.BombOrb;
+import M10Robot.orbs.PresentOrb;
+import M10Robot.orbs.SearchlightOrb;
+import com.megacrit.cardcrawl.actions.defect.ChannelAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.orbs.AbstractOrb;
+
+import java.util.ArrayList;
+
+import static M10Robot.M10RobotMod.makeCardPath;
+
+public class QuickBoot extends AbstractDynamicCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = M10RobotMod.makeID(QuickBoot.class.getSimpleName());
+    public static final String IMG = makeCardPath("Boot.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
+
+    private static final int COST = 2;
+    private static final int UPGRADE_COST = 1;
+    private static final int ORBS = 3;
+
+    // /STAT DECLARATION/
+
+    public QuickBoot() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        magicNumber = baseMagicNumber = ORBS;
+        isInnate = true;
+        exhaust = true;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        for (int i = 0 ; i < magicNumber ; i++) {
+            ArrayList<AbstractOrb> orbs = new ArrayList<>();
+            orbs.add(new BitOrb());
+            orbs.add(new SearchlightOrb());
+            orbs.add(new BombOrb());
+            orbs.add(new PresentOrb());
+            this.addToBot(new ChannelAction(orbs.get(AbstractDungeon.cardRandomRng.random(orbs.size() - 1))));
+        }
+
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBaseCost(UPGRADE_COST);
+            initializeDescription();
+        }
+    }
+}
