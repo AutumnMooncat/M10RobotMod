a96ad4e1a7dc0b95cb42a14f15daea5b7c430dd1
Alison Turner <alisonmoons@gmail.com>
2021-12-30 14:31:37 -0400

    Quick Repair now provides Plated Armor instead of Temp HP

--- TheM10Robot/src/main/java/M10Robot/cards/QuickRepair.java
+++ TheM10Robot/src/main/java/M10Robot/cards/QuickRepair.java
@@ -3,10 +3,11 @@ package M10Robot.cards;
 import M10Robot.M10RobotMod;
 import M10Robot.cards.abstractCards.AbstractDynamicCard;
 import M10Robot.characters.M10Robot;
-import com.evacipated.cardcrawl.mod.stslib.actions.tempHp.AddTemporaryHPAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.PlatedArmorPower;
 
 import static M10Robot.M10RobotMod.makeCardPath;
 
@@ -37,8 +38,8 @@ public class QuickRepair extends AbstractDynamicCard {
     private static final int COST = 0;
     private static final int BLOCK = 6;
     private static final int UPGRADE_PLUS_BLOCK = 4;
-    private static final int EFFECT = 1;
-    private static final int UPGRADE_PLUS_EFFECT = 1;
+    private static final int PLATED = 3;
+    private static final int UPGRADE_PLUS_PLATED = 2;
 
     // /STAT DECLARATION/
 
@@ -46,6 +47,7 @@ public class QuickRepair extends AbstractDynamicCard {
     public QuickRepair() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         block = baseBlock = BLOCK;
+        magicNumber = baseMagicNumber = PLATED;
         exhaust = true;
     }
 
@@ -53,7 +55,7 @@ public class QuickRepair extends AbstractDynamicCard {
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         this.addToBot(new GainBlockAction(p, block));
-        this.addToBot(new AddTemporaryHPAction(p, p, block/2));
+        this.addToBot(new ApplyPowerAction(p, p, new PlatedArmorPower(p, magicNumber)));
     }
 
     //Upgraded stats.
@@ -62,6 +64,7 @@ public class QuickRepair extends AbstractDynamicCard {
         if (!upgraded) {
             upgradeName();
             upgradeBlock(UPGRADE_PLUS_BLOCK);
+            upgradeMagicNumber(UPGRADE_PLUS_PLATED);
             initializeDescription();
         }
     }
