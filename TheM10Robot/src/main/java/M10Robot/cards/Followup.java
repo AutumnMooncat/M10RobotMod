7fc8dc114092f285d1b088c5e1d2ee52d8347390
Alison Turner <alisonmoons@gmail.com>
2021-12-30 11:35:00 -0400

    Added Followup, Overclocks the next card played

---
+++ TheM10Robot/src/main/java/M10Robot/cards/Followup.java
@@ -0,0 +1,60 @@
+package M10Robot.cards;
+
+import M10Robot.M10RobotMod;
+import M10Robot.cards.abstractCards.AbstractDynamicCard;
+import M10Robot.characters.M10Robot;
+import M10Robot.powers.FollowupPower;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static M10Robot.M10RobotMod.makeCardPath;
+
+public class Followup extends AbstractDynamicCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = M10RobotMod.makeID(Followup.class.getSimpleName());
+    public static final String IMG = makeCardPath("Followup.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.ALL;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
+
+    private static final int COST = 0;
+    private static final int STACKS = 1;
+    private static final int UPGRADE_PLUS_STACKS = 1;
+    private static final int EFFECT = 2;
+    private static final int UPGRADE_PLUS_EFFECT = 1;
+
+    // /STAT DECLARATION/
+
+
+    public Followup() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        magicNumber = baseMagicNumber = STACKS;
+        secondMagicNumber = baseSecondMagicNumber = EFFECT;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new ApplyPowerAction(p, p, new FollowupPower(p, magicNumber, secondMagicNumber)));
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_PLUS_STACKS);
+            initializeDescription();
+        }
+    }
+}
---
+++ TheM10Robot/src/main/java/M10Robot/powers/FollowupPower.java
@@ -0,0 +1,83 @@
+package M10Robot.powers;
+
+import M10Robot.M10RobotMod;
+import M10Robot.actions.OverclockCardAction;
+import M10Robot.util.OverclockUtil;
+import basemod.interfaces.CloneablePowerInterface;
+import com.evacipated.cardcrawl.mod.stslib.powers.abstracts.TwoAmountPower;
+import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.NonStackablePower;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+public class FollowupPower extends TwoAmountPower implements CloneablePowerInterface, NonStackablePower {
+
+    public static final String POWER_ID = M10RobotMod.makeID("FollowupPower");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
+    // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
+    //private static final Texture tex84 = TextureLoader.getTexture(makePowerPath("placeholder_power84.png"));
+    //private static final Texture tex32 = TextureLoader.getTexture(makePowerPath("placeholder_power32.png"));
+
+    public FollowupPower(AbstractCreature owner, int cards, int overclocks) {
+        this.name = NAME;
+        this.ID = POWER_ID;
+        this.owner = owner;
+        this.amount = cards;
+        this.amount2 = overclocks;
+
+        this.type = PowerType.BUFF;
+        this.isTurnBased = false;
+
+        // We load those txtures here.
+        //this.loadRegion("cExplosion");
+        this.loadRegion("burst");
+        //this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        //this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+
+        updateDescription();
+    }
+
+    public void onUseCard(AbstractCard card, UseCardAction action) {
+        if (!card.purgeOnUse && this.amount > 0 && OverclockUtil.canOverclock(card)) {
+            this.flash();
+            this.addToBot(new OverclockCardAction(card, amount2));
+            --this.amount;
+            if (this.amount == 0) {
+                this.addToTop(new RemoveSpecificPowerAction(this.owner, this.owner, this));
+            } else {
+                updateDescription();
+            }
+        }
+
+    }
+
+    public void updateDescription() {
+        if (amount == 1) {
+            if (amount2 == 1) {
+                description = DESCRIPTIONS[0] + amount2 + DESCRIPTIONS[1];
+            } else {
+                description = DESCRIPTIONS[0] + amount2 + DESCRIPTIONS[2];
+            }
+        } else {
+            if (amount2 == 1) {
+                description = DESCRIPTIONS[3] + amount + DESCRIPTIONS[4] + amount2 + DESCRIPTIONS[1];
+            } else {
+                description = DESCRIPTIONS[3] + amount + DESCRIPTIONS[4] + amount2 + DESCRIPTIONS[2];
+            }
+        }
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new FollowupPower(owner, amount, amount2);
+    }
+
+}
