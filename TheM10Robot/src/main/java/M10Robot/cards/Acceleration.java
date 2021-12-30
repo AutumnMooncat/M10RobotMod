394e63db11fe34e758e18ea1e73bdc343f28343b
Alison Turner <alisonmoons@gmail.com>
2021-12-30 11:36:58 -0400

    Added Accelerations, Overclocks and Reshuffles next cards played

---
+++ TheM10Robot/src/main/java/M10Robot/cards/Acceleration.java
@@ -0,0 +1,63 @@
+package M10Robot.cards;
+
+import M10Robot.M10RobotMod;
+import M10Robot.cards.abstractCards.AbstractDynamicCard;
+import M10Robot.characters.M10Robot;
+import M10Robot.powers.FollowupPower;
+import M10Robot.powers.ReshufflePower;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static M10Robot.M10RobotMod.makeCardPath;
+
+public class Acceleration extends AbstractDynamicCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = M10RobotMod.makeID(Acceleration.class.getSimpleName());
+    public static final String IMG = makeCardPath("Acceleration.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.ALL;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int STACKS = 2;
+    private static final int UPGRADE_PLUS_STACKS = 1;
+    private static final int EFFECT = 2;
+    private static final int UPGRADE_PLUS_EFFECT = 1;
+
+    // /STAT DECLARATION/
+
+
+    public Acceleration() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        magicNumber = baseMagicNumber = STACKS;
+        secondMagicNumber = baseSecondMagicNumber = EFFECT;
+        exhaust = true;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new ApplyPowerAction(p, p, new FollowupPower(p, magicNumber, secondMagicNumber)));
+        this.addToBot(new ApplyPowerAction(p, p, new ReshufflePower(p, magicNumber)));
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            exhaust = false;
+            initializeDescription();
+        }
+    }
+}
---
+++ TheM10Robot/src/main/java/M10Robot/powers/ReshufflePower.java
@@ -0,0 +1,85 @@
+package M10Robot.powers;
+
+import M10Robot.M10RobotMod;
+import basemod.interfaces.CloneablePowerInterface;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+public class ReshufflePower extends AbstractPower implements CloneablePowerInterface {
+
+    public static final String POWER_ID = M10RobotMod.makeID("ReshufflePower");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    private boolean justEvoked = true;
+
+    // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
+    // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
+    //private static final Texture tex84 = TextureLoader.getTexture(makePowerPath("placeholder_power84.png"));
+    //private static final Texture tex32 = TextureLoader.getTexture(makePowerPath("placeholder_power32.png"));
+
+    public ReshufflePower(AbstractCreature owner, int amount) {
+        this.name = NAME;
+        this.ID = POWER_ID;
+        this.owner = owner;
+        this.amount = amount;
+
+        this.type = PowerType.BUFF;
+        this.isTurnBased = false;
+
+        // We load those txtures here.
+        //this.loadRegion("cExplosion");
+        this.loadRegion("rebound");
+        //this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        //this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+
+        updateDescription();
+    }
+
+    public void updateDescription() {
+        if (amount == 1) {
+            this.description = DESCRIPTIONS[0];
+        } else {
+            this.description = DESCRIPTIONS[1] + this.amount + DESCRIPTIONS[2];
+        }
+    }
+
+    public void onAfterUseCard(AbstractCard card, UseCardAction action) {
+        if (this.justEvoked) {
+            this.justEvoked = false;
+        } else {
+            //Don't waste a use if the card already was going to return
+            if (!card.shuffleBackIntoDrawPile && card.type != AbstractCard.CardType.POWER) {
+                this.flash();
+                //action.returnToHand = true;
+                card.shuffleBackIntoDrawPile = true;
+                this.addToTop(new AbstractGameAction() {
+                    @Override
+                    public void update() {
+                        card.shuffleBackIntoDrawPile = false;
+                        this.isDone = true;
+                    }
+                });
+            }
+            if (this.amount == 1) {
+                this.addToBot(new RemoveSpecificPowerAction(owner, owner, this));
+            } else {
+                this.addToBot(new ReducePowerAction(owner, owner, this, 1));
+            }
+        }
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new ReshufflePower(owner, amount);
+    }
+
+}
