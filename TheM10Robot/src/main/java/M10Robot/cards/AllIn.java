235c9cba5f767e275adadc81fc3803d60c6c25b7
Alison Turner <alisonmoons@gmail.com>
2021-12-31 21:15:18 -0400

    Added All In - Overclocks all cards in your hand X times

---
+++ TheM10Robot/src/main/java/M10Robot/cards/AllIn.java
@@ -0,0 +1,102 @@
+package M10Robot.cards;
+
+import M10Robot.M10RobotMod;
+import M10Robot.actions.OverclockCardAction;
+import M10Robot.cards.abstractCards.AbstractDynamicCard;
+import M10Robot.characters.M10Robot;
+import basemod.interfaces.XCostModifier;
+import basemod.patches.com.megacrit.cardcrawl.cards.AbstractCard.CardModifierPatches;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.relics.ChemicalX;
+import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static M10Robot.M10RobotMod.makeCardPath;
+
+public class AllIn extends AbstractDynamicCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = M10RobotMod.makeID(AllIn.class.getSimpleName());
+    public static final String IMG = makeCardPath("AllIn.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
+
+    private static final int COST = -1;
+    private static final int BONUS_HITS = 0;
+    private static final int UPGRADE_PLUS_BONUS_HITS = 1;
+
+    // /STAT DECLARATION/
+
+
+    public AllIn() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        magicNumber = baseMagicNumber = BONUS_HITS;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        int effect = EnergyPanel.totalCount;
+
+        if (this.energyOnUse != -1) {
+            effect = this.energyOnUse;
+        }
+
+        if (p.hasRelic("Chemical X")) {
+            effect += ChemicalX.BOOST;
+            p.getRelic("Chemical X").flash();
+        }
+
+        ArrayList<List<?>> lists = new ArrayList<>();
+        lists.add(AbstractDungeon.player.hand.group);
+        lists.add(AbstractDungeon.player.drawPile.group);
+        lists.add(AbstractDungeon.player.discardPile.group);
+        lists.add(AbstractDungeon.player.powers);
+        lists.add(AbstractDungeon.player.relics);
+        lists.add(CardModifierPatches.CardModifierFields.cardModifiers.get(this));
+        for (List<?> list : lists) {
+            for (Object item : list) {
+                if (item instanceof XCostModifier) {
+                    XCostModifier mod = (XCostModifier)item;
+                    if (mod.xCostModifierActive(this)) {
+                        effect += mod.modifyX(this);
+                    }
+                }
+            }
+        }
+
+        effect += magicNumber;
+
+        if (effect > 0) {
+            this.addToBot(new OverclockCardAction(true, effect));
+        }
+
+        if (!this.freeToPlayOnce) {
+            p.energy.use(EnergyPanel.totalCount);
+        }
+
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_PLUS_BONUS_HITS);
+            initializeDescription();
+        }
+    }
+}
