a1b9ef613fd042d3e8353913180afa5e6c478f0e
Alison Turner <alisonmoons@gmail.com>
2022-01-03 10:52:38 -0400

    Momentum testing stuff

--- TheM10Robot/src/main/java/M10Robot/powers/MomentumPower.java
+++ TheM10Robot/src/main/java/M10Robot/powers/MomentumPower.java
@@ -2,14 +2,17 @@ package M10Robot.powers;
 
 import M10Robot.M10RobotMod;
 import basemod.interfaces.CloneablePowerInterface;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.unlock.UnlockTracker;
 
 public class MomentumPower extends AbstractPower implements CloneablePowerInterface {
 
@@ -37,12 +40,40 @@ public class MomentumPower extends AbstractPower implements CloneablePowerInterf
         this.loadRegion("rupture");
         //this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
         //this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+        this.canGoNegative = true;
 
         updateDescription();
     }
 
+    public void onInitialApplication() {
+        AbstractDungeon.player.gameHandSize += amount;
+    }
+
+    public void onRemove() {
+        AbstractDungeon.player.gameHandSize -= amount;
+    }
+
+
+    public void stackPower(int stackAmount) {
+        this.fontScale = 8.0F;
+        this.amount += stackAmount;
+        AbstractDungeon.player.gameHandSize += stackAmount;
+        if (this.amount == 0) {
+            this.addToTop(new RemoveSpecificPowerAction(this.owner, this.owner, this));
+        }
+    }
+
+    public void reducePower(int reduceAmount) {
+        this.fontScale = 8.0F;
+        this.amount -= reduceAmount;
+        AbstractDungeon.player.gameHandSize -= reduceAmount;
+        if (this.amount == 0) {
+            this.addToTop(new RemoveSpecificPowerAction(this.owner, this.owner, this));
+        }
+    }
+
     public float atDamageGive(float damage, DamageInfo.DamageType type) {
-        return type == DamageInfo.DamageType.NORMAL ? damage + (float)this.amount : damage;
+        return type == DamageInfo.DamageType.NORMAL ? ((damage += (float)this.amount) < 0.0F ? 0.0F : damage) : damage;
     }
 
     public float modifyBlock(float blockAmount) {
@@ -51,16 +82,24 @@ public class MomentumPower extends AbstractPower implements CloneablePowerInterf
 
     public void atStartOfTurnPostDraw() {
         this.flash();
-        this.addToBot(new DrawCardAction(this.owner, this.amount));
-        if (this.amount == 0) {
-            this.addToBot(new RemoveSpecificPowerAction(this.owner, this.owner, this));
+        if (this.amount == 1 || this.amount == -1) {
+            this.addToBot(new RemoveSpecificPowerAction(owner, owner, this));
+        } else if (this.amount > 0) {
+            this.addToBot(new ReducePowerAction(owner, owner, this, 1));
         } else {
-            this.addToBot(new ReducePowerAction(this.owner, this.owner, this, 1));
+            this.addToBot(new ApplyPowerAction(owner, owner, new MomentumPower(owner, 1)));
         }
     }
 
     public void updateDescription() {
-        this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[1];
+        if (this.amount > 0) {
+            this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[1];
+            this.type = PowerType.BUFF;
+        } else {
+            this.description = DESCRIPTIONS[2] + (-this.amount) + DESCRIPTIONS[3];
+            this.type = PowerType.DEBUFF;
+        }
+
     }
 
     @Override
