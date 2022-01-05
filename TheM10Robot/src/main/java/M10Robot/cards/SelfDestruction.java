56fa94b5e91940208c3166dff8ca1eaf2d68801e
Alison Turner <alisonmoons@gmail.com>
2022-01-05 00:59:06 -0400

    Self Destruct now a single target attack that applies Stun

--- TheM10Robot/src/main/java/M10Robot/cards/SelfDestruction.java
+++ TheM10Robot/src/main/java/M10Robot/cards/SelfDestruction.java
@@ -1,16 +1,29 @@
 package M10Robot.cards;
 
 import M10Robot.M10RobotMod;
-import M10Robot.actions.SelfDestructAction;
+import M10Robot.actions.SelfDestructAction2;
 import M10Robot.cardModifiers.HeavyModifier;
-import M10Robot.cardModifiers.SpikyModifier;
 import M10Robot.cards.abstractCards.AbstractDynamicCard;
 import M10Robot.characters.M10Robot;
 import M10Robot.patches.RestorePositionPatches;
+import M10Robot.vfx.BurnToAshEffect;
 import basemod.helpers.CardModifierManager;
+import com.badlogic.gdx.graphics.Color;
+import com.evacipated.cardcrawl.mod.stslib.actions.common.StunMonsterAction;
+import com.evacipated.cardcrawl.mod.stslib.patches.NeutralPowertypePatch;
+import com.evacipated.cardcrawl.mod.stslib.powers.StunMonsterPower;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.ScreenShake;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.ExplosionSmallEffect;
+import com.megacrit.cardcrawl.vfx.combat.FlashAtkImgEffect;
+import com.megacrit.cardcrawl.vfx.combat.ShockWaveEffect;
 
 import static M10Robot.M10RobotMod.makeCardPath;
 
@@ -32,17 +45,16 @@ public class SelfDestruction extends AbstractDynamicCard {
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
 
-    private static final int COST = 2;
+    private static final int COST = 3;
     private static final int DAMAGE = 20;
-    private static final int UPGRADE_PLUS_DMG = 10;
+    private static final int UPGRADE_PLUS_DAMAGE = 8;
 
     // /STAT DECLARATION/
 
     public SelfDestruction() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
-        baseDamage = damage = DAMAGE;
-        isMultiDamage = true;
-        CardModifierManager.addModifier(this, new SpikyModifier());
+        damage = baseDamage = DAMAGE;
+        exhaust = true;
         CardModifierManager.addModifier(this, new HeavyModifier());
     }
 
@@ -52,18 +64,21 @@ public class SelfDestruction extends AbstractDynamicCard {
         this.addToBot(new AbstractGameAction() {
             @Override
             public void update() {
-                RestorePositionPatches.setBackUp(p, p.drawX, p.drawY, p.hb.cX, p.hb.cY);
+                AbstractDungeon.effectsQueue.add(new ExplosionSmallEffect(m.hb.cX, m.hb.cY));
+                AbstractDungeon.effectsQueue.add(new BurnToAshEffect(m.hb.cX, m.hb.cY));
+                CardCrawlGame.screenShake.shake(ScreenShake.ShakeIntensity.HIGH, ScreenShake.ShakeDur.MED, false);
+                CardCrawlGame.sound.play("ORB_LIGHTNING_EVOKE", 0.15f);
+                CardCrawlGame.sound.play("GHOST_ORB_IGNITE_2", 0.15f);
+                CardCrawlGame.sound.play("WATCHER_HEART_PUNCH", 0.15f);
+                AbstractDungeon.effectsQueue.add(new ShockWaveEffect(m.hb.cX, m.hb.cY, Color.RED.cpy(), ShockWaveEffect.ShockWaveType.CHAOTIC));
                 this.isDone = true;
             }
         });
-        this.addToBot(new SelfDestructAction(p, m, multiDamage, damageTypeForTurn, 0, false));
-        this.addToBot(new AbstractGameAction() {
-            @Override
-            public void update() {
-                RestorePositionPatches.removeBackUp(p);
-                this.isDone = true;
-            }
-        });
+        this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.NONE, true));
+        //We really don't want this to interact with Stasis
+        StunMonsterPower pow = new StunMonsterPower(m, 1);
+        pow.type = NeutralPowertypePatch.NEUTRAL;
+        this.addToBot(new ApplyPowerAction(m, p, pow, 1));
     }
 
     // Upgraded stats.
@@ -71,7 +86,7 @@ public class SelfDestruction extends AbstractDynamicCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeDamage(UPGRADE_PLUS_DMG);
+            upgradeDamage(UPGRADE_PLUS_DAMAGE);
             initializeDescription();
         }
     }
