7b56917f12ab1a59c18270df733c7430eb713a8d
Alison Turner <alisonmoons@gmail.com>
2022-01-04 20:57:38 -0400

    Power Savings now gives Weak instead of Scramble

--- TheM10Robot/src/main/java/M10Robot/cards/PowerSavings.java
+++ TheM10Robot/src/main/java/M10Robot/cards/PowerSavings.java
@@ -8,6 +8,7 @@ import M10Robot.powers.ScrambledPower;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.WeakPower;
 
 import static M10Robot.M10RobotMod.makeCardPath;
 
@@ -49,14 +50,13 @@ public class PowerSavings extends AbstractDynamicCard {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         magicNumber = baseMagicNumber = ENERGY;
         secondMagicNumber = baseSecondMagicNumber = WEAK;
-        initializeDescription();
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         this.addToBot(new ApplyPowerAction(p, p, new PowerSavingsPower(p, magicNumber)));
-        this.addToBot(new ApplyPowerAction(p, p, new ScrambledPower(p, secondMagicNumber)));
+        this.addToBot(new ApplyPowerAction(p, p, new WeakPower(p, secondMagicNumber, false)));
     }
 
     //Upgraded stats.
@@ -67,8 +67,7 @@ public class PowerSavings extends AbstractDynamicCard {
             //upgradeMagicNumber(UPGRADE_PLUS_ENERGY);
             upgradeSecondMagicNumber(UPGRADE_PLUS_WEAK);
             if (baseSecondMagicNumber < 0) {
-                baseSecondMagicNumber = 0;
-                secondMagicNumber = baseSecondMagicNumber;
+                baseSecondMagicNumber = secondMagicNumber = 0;
             }
             initializeDescription();
         }
