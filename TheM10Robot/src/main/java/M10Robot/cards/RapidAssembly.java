931cb859b3cffb1242f2b09d7a257c58e9e2380f
Alison Turner <alisonmoons@gmail.com>
2022-01-05 23:42:36 -0400

    Reimplemented RapidAssembly. Now Overclocks all cards in your hand 2(3) times and exhausts.

--- TheM10Robot/src/main/java/M10Robot/cards/RapidAssembly.java
+++ TheM10Robot/src/main/java/M10Robot/cards/RapidAssembly.java
@@ -1,8 +1,9 @@
-package M10Robot.cutStuff;
+package M10Robot.cards;
 
 import M10Robot.M10RobotMod;
+import M10Robot.actions.OverclockCardAction;
+import M10Robot.cards.abstractCards.AbstractDynamicCard;
 import M10Robot.cards.abstractCards.AbstractSwappableCard;
-import M10Robot.cards.uniqueCards.UniqueCard;
 import M10Robot.characters.M10Robot;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
@@ -11,7 +12,7 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static M10Robot.M10RobotMod.makeCardPath;
 
-public class RapidAssembly extends AbstractSwappableCard implements UniqueCard {
+public class RapidAssembly extends AbstractDynamicCard {
 
     // TEXT DECLARATION
 
@@ -23,39 +24,27 @@ public class RapidAssembly extends AbstractSwappableCard implements UniqueCard {
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
 
     private static final int COST = 0;
-    private static final int DRAW = 2;
-    private static final int UPGRADE_PLUS_DRAW = 1;
-    private static final int E = 1;
+    private static final int EFFECT = 2;
+    private static final int UPGRADE_PLUS_EFFECT = 1;
 
     // /STAT DECLARATION/
 
     public RapidAssembly() {
-        this(null);
-    }
-
-    public RapidAssembly(AbstractSwappableCard linkedCard) {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
-        magicNumber = baseMagicNumber = DRAW;
-        secondMagicNumber = baseSecondMagicNumber = E;
+        magicNumber = baseMagicNumber = EFFECT;
         this.exhaust = true;
-        if (linkedCard == null) {
-            setLinkedCard(new PerpetualAssembly(this));
-        } else {
-            setLinkedCard(linkedCard);
-        }
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new DrawCardAction(magicNumber));
-        this.addToBot(new GainEnergyAction(secondMagicNumber));
+        this.addToBot(new OverclockCardAction(true, magicNumber));
     }
 
     //Upgraded stats.
@@ -63,9 +52,8 @@ public class RapidAssembly extends AbstractSwappableCard implements UniqueCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeSecondMagicNumber(UPGRADE_PLUS_DRAW);
+            upgradeMagicNumber(UPGRADE_PLUS_EFFECT);
             initializeDescription();
-            super.upgrade();
         }
     }
 }
--- TheM10Robot/src/main/java/M10Robot/cutStuff/PerpetualAssembly.java
+++ TheM10Robot/src/main/java/M10Robot/cutStuff/PerpetualAssembly.java
@@ -1,73 +1,73 @@
-package M10Robot.cutStuff;
-
-import M10Robot.M10RobotMod;
-import M10Robot.actions.MultichannelAction;
-import M10Robot.cards.abstractCards.AbstractSwappableCard;
-import M10Robot.characters.M10Robot;
-import M10Robot.orbs.PresentOrb;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.DrawCardNextTurnPower;
-
-import static M10Robot.M10RobotMod.makeCardPath;
-
-public class PerpetualAssembly extends AbstractSwappableCard {
-
-    // TEXT DECLARATION
-
-    public static final String ID = M10RobotMod.makeID(PerpetualAssembly.class.getSimpleName());
-    public static final String IMG = makeCardPath("PerpetualAssembly.png");
-
-    // /TEXT DECLARATION/
-
-
-    // STAT DECLARATION
-
-    private static final CardRarity RARITY = CardRarity.COMMON;
-    private static final CardTarget TARGET = CardTarget.SELF;
-    private static final CardType TYPE = CardType.SKILL;
-    public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
-
-    private static final int COST = 1;
-    private static final int UPGRADE_COST = 0;
-    private static final int ORBS = 1;
-    private static final int CARDS = 1;
-    private static final int UPGRADE_PLUS_CARDS = 1;
-
-    // /STAT DECLARATION/
-
-    public PerpetualAssembly() {
-        this(null);
-    }
-
-    public PerpetualAssembly(AbstractSwappableCard linkedCard) {
-        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
-        magicNumber = baseMagicNumber = ORBS;
-        secondMagicNumber = baseSecondMagicNumber = CARDS;
-        if (linkedCard == null) {
-            setLinkedCard(new RapidAssembly(this));
-        } else {
-            setLinkedCard(linkedCard);
-        }
-    }
-
-    // Actions the card should do.
-    @Override
-    public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new MultichannelAction(new PresentOrb(), magicNumber));
-        this.addToBot(new ApplyPowerAction(p, p, new DrawCardNextTurnPower(p, secondMagicNumber)));
-    }
-
-    //Upgraded stats.
-    @Override
-    public void upgrade() {
-        if (!upgraded) {
-            upgradeName();
-            //upgradeBaseCost(UPGRADE_COST);
-            upgradeSecondMagicNumber(UPGRADE_PLUS_CARDS);
-            initializeDescription();
-            super.upgrade();
-        }
-    }
-}
+//package M10Robot.cutStuff;
+//
+//import M10Robot.M10RobotMod;
+//import M10Robot.actions.MultichannelAction;
+//import M10Robot.cards.abstractCards.AbstractSwappableCard;
+//import M10Robot.characters.M10Robot;
+//import M10Robot.orbs.PresentOrb;
+//import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+//import com.megacrit.cardcrawl.characters.AbstractPlayer;
+//import com.megacrit.cardcrawl.monsters.AbstractMonster;
+//import com.megacrit.cardcrawl.powers.DrawCardNextTurnPower;
+//
+//import static M10Robot.M10RobotMod.makeCardPath;
+//
+//public class PerpetualAssembly extends AbstractSwappableCard {
+//
+//    // TEXT DECLARATION
+//
+//    public static final String ID = M10RobotMod.makeID(PerpetualAssembly.class.getSimpleName());
+//    public static final String IMG = makeCardPath("PerpetualAssembly.png");
+//
+//    // /TEXT DECLARATION/
+//
+//
+//    // STAT DECLARATION
+//
+//    private static final CardRarity RARITY = CardRarity.COMMON;
+//    private static final CardTarget TARGET = CardTarget.SELF;
+//    private static final CardType TYPE = CardType.SKILL;
+//    public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
+//
+//    private static final int COST = 1;
+//    private static final int UPGRADE_COST = 0;
+//    private static final int ORBS = 1;
+//    private static final int CARDS = 1;
+//    private static final int UPGRADE_PLUS_CARDS = 1;
+//
+//    // /STAT DECLARATION/
+//
+//    public PerpetualAssembly() {
+//        this(null);
+//    }
+//
+//    public PerpetualAssembly(AbstractSwappableCard linkedCard) {
+//        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+//        magicNumber = baseMagicNumber = ORBS;
+//        secondMagicNumber = baseSecondMagicNumber = CARDS;
+//        if (linkedCard == null) {
+//            setLinkedCard(new RapidAssembly(this));
+//        } else {
+//            setLinkedCard(linkedCard);
+//        }
+//    }
+//
+//    // Actions the card should do.
+//    @Override
+//    public void use(AbstractPlayer p, AbstractMonster m) {
+//        this.addToBot(new MultichannelAction(new PresentOrb(), magicNumber));
+//        this.addToBot(new ApplyPowerAction(p, p, new DrawCardNextTurnPower(p, secondMagicNumber)));
+//    }
+//
+//    //Upgraded stats.
+//    @Override
+//    public void upgrade() {
+//        if (!upgraded) {
+//            upgradeName();
+//            //upgradeBaseCost(UPGRADE_COST);
+//            upgradeSecondMagicNumber(UPGRADE_PLUS_CARDS);
+//            initializeDescription();
+//            super.upgrade();
+//        }
+//    }
+//}
