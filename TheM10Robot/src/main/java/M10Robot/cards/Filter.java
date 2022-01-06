8a2e4989b9638f33d42a94a1c9744a2a2d73a71c
Alison Turner <alisonmoons@gmail.com>
2022-01-06 14:21:24 -0400

    Filter now draws, discards non-attacks, and gives energy for each discarded card

--- TheM10Robot/src/main/java/M10Robot/cards/Filter.java
+++ TheM10Robot/src/main/java/M10Robot/cards/Filter.java
@@ -5,8 +5,11 @@ import M10Robot.cards.abstractCards.AbstractDynamicCard;
 import M10Robot.characters.M10Robot;
 import com.evacipated.cardcrawl.mod.stslib.actions.common.SelectCardsInHandAction;
 import com.evacipated.cardcrawl.mod.stslib.cards.interfaces.BranchingUpgradesCard;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.actions.common.ExhaustSpecificCardAction;
 import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.actions.unique.UnloadAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -44,26 +47,31 @@ public class Filter extends AbstractDynamicCard implements BranchingUpgradesCard
 
     private static final int COST = 1;
     private static final int UPGRADE_COST = 0;
+    private static final int DRAW = 1;
+    private static final int UPGRADE_PLUS_DRAW = 1;
 
     // /STAT DECLARATION/
 
 
     public Filter() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        magicNumber = baseMagicNumber = DRAW;
+        exhaust = true;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        Consumer<List<AbstractCard>> plsWork = this::exhaustEnergize;
-        this.addToBot(new SelectCardsInHandAction(99, EXTENDED_DESCRIPTION[0], true, true, Objects::nonNull, plsWork));
-    }
-
-    private void exhaustEnergize(List<AbstractCard> l) {
-        this.addToBot(new GainEnergyAction(l.size()));
-        for (AbstractCard c : l) {
-            this.addToBot(new ExhaustSpecificCardAction(c, AbstractDungeon.player.hand));
-        }
+        this.addToBot(new DrawCardAction(magicNumber));
+        this.addToBot(new AbstractGameAction() {
+            @Override
+            public void update() {
+                int nonAttacks = (int) p.hand.group.stream().filter(c -> c.type != CardType.ATTACK).count();
+                this.addToTop(new GainEnergyAction(nonAttacks));
+                this.isDone = true;
+            }
+        });
+        this.addToBot(new UnloadAction(p));
     }
 
     //Upgraded stats.
@@ -85,6 +93,6 @@ public class Filter extends AbstractDynamicCard implements BranchingUpgradesCard
     }
 
     public void branchUpgrade() {
-        this.selfRetain = true;
+        upgradeMagicNumber(UPGRADE_PLUS_DRAW);
     }
 }
