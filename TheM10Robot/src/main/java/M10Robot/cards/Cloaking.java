89bbf94dd53f8fbbf6b3668021d5b9c68aa94457
Alison Turner <alisonmoons@gmail.com>
2022-01-06 13:44:47 -0400

    Added Cloaking, gives Block and Vigor

---
+++ TheM10Robot/src/main/java/M10Robot/cards/Cloaking.java
@@ -0,0 +1,71 @@
+package M10Robot.cards;
+
+import M10Robot.M10RobotMod;
+import M10Robot.cards.abstractCards.AbstractDynamicCard;
+import M10Robot.characters.M10Robot;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.PlatedArmorPower;
+import com.megacrit.cardcrawl.powers.watcher.VigorPower;
+
+import static M10Robot.M10RobotMod.makeCardPath;
+
+public class Cloaking extends AbstractDynamicCard {
+
+    /*
+     * Wiki-page: https://github.com/daviscook477/BaseMod/wiki/Custom-Cards
+     *
+     * Defend Gain 5 (8) block.
+     */
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = M10RobotMod.makeID(Cloaking.class.getSimpleName());
+    public static final String IMG = makeCardPath("Cloaking.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
+
+    private static final int COST = 2;
+    private static final int BLOCK = 8;
+    private static final int UPGRADE_PLUS_BLOCK = 4;
+    private static final int VIGOR = 5;
+    private static final int UPGRADE_PLUS_VIGOR = 2;
+
+    // /STAT DECLARATION/
+
+
+    public Cloaking() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        block = baseBlock = BLOCK;
+        magicNumber = baseMagicNumber = VIGOR;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new GainBlockAction(p, block));
+        this.addToBot(new ApplyPowerAction(p, p, new VigorPower(p, magicNumber)));
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBlock(UPGRADE_PLUS_BLOCK);
+            upgradeMagicNumber(UPGRADE_PLUS_VIGOR);
+            initializeDescription();
+        }
+    }
+}
