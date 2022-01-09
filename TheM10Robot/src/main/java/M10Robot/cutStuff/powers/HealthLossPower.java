e0f8fee1abb405aeb19331173b7050a59386883c
Alison Turner <alisonmoons@gmail.com>
2022-01-09 12:26:58 -0400

    Added HealthLossPower but removed it

---
+++ TheM10Robot/src/main/java/M10Robot/cutStuff/powers/HealthLossPower.java
@@ -0,0 +1,62 @@
+package M10Robot.cutStuff.powers;
+
+import M10Robot.M10RobotMod;
+import M10Robot.actions.FasterLoseHPAction;
+import basemod.interfaces.CloneablePowerInterface;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
+
+public class HealthLossPower extends AbstractPower implements CloneablePowerInterface {
+
+    public static final String POWER_ID = M10RobotMod.makeID("HealthLossPower");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+    private AbstractCreature source;
+
+    // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
+    // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
+    //private static final Texture tex84 = TextureLoader.getTexture(makePowerPath("placeholder_power84.png"));
+    //private static final Texture tex32 = TextureLoader.getTexture(makePowerPath("placeholder_power32.png"));
+
+    public HealthLossPower(AbstractCreature owner, AbstractCreature source, int amount) {
+        this.name = NAME;
+        this.ID = POWER_ID;
+        this.owner = owner;
+        this.amount = amount;
+        this.source = source;
+
+        this.type = PowerType.DEBUFF;
+        this.isTurnBased = false;
+
+        // We load those txtures here.
+        //this.loadRegion("cExplosion");
+        this.loadRegion("brutality");
+        //this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        //this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+
+        updateDescription();
+    }
+
+    public void atStartOfTurn() {
+        if (AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT && !AbstractDungeon.getMonsters().areMonstersBasicallyDead()) {
+            this.flashWithoutSound();
+            this.addToBot(new FasterLoseHPAction(owner, source, amount, AbstractGameAction.AttackEffect.FIRE));
+        }
+    }
+
+    public void updateDescription() {
+        this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[1];
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new HealthLossPower(owner, source, amount);
+    }
+
+}
