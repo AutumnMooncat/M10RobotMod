08152932735c27ce5507dd3aa5c8c4591c1cb555
Alison Turner <alisonmoons@gmail.com>
2022-01-27 14:38:18 -0400

    Acceleration and Follow Up now properly target the player

--- TheM10Robot/src/main/java/M10Robot/cards/Acceleration.java
+++ TheM10Robot/src/main/java/M10Robot/cards/Acceleration.java
@@ -24,7 +24,7 @@ public class Acceleration extends AbstractDynamicCard {
     // STAT DECLARATION
 
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
-    private static final CardTarget TARGET = CardTarget.ALL;
+    private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
 
--- TheM10Robot/src/main/java/M10Robot/cards/Followup.java
+++ TheM10Robot/src/main/java/M10Robot/cards/Followup.java
@@ -23,7 +23,7 @@ public class Followup extends AbstractDynamicCard {
     // STAT DECLARATION
 
     private static final CardRarity RARITY = CardRarity.COMMON;
-    private static final CardTarget TARGET = CardTarget.ALL;
+    private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
 
