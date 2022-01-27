95f141e203e0ed7065be816b4731b2fcc0d23eec
Alison Turner <alisonmoons@gmail.com>
2022-01-27 16:45:08 -0400

    Reloadable cards now have red title and energy when they need to be reloaded. Attempting to play the card will now show a can't play message

--- TheM10Robot/src/main/java/M10Robot/cards/abstractCards/AbstractReloadableCard.java
+++ TheM10Robot/src/main/java/M10Robot/cards/abstractCards/AbstractReloadableCard.java
@@ -1,19 +1,25 @@
 package M10Robot.cards.abstractCards;
 
+import M10Robot.M10RobotMod;
 import M10Robot.cardModifiers.ReloadingModifier;
 import M10Robot.cards.interfaces.OnDiscardedCard;
 import M10Robot.powers.interfaces.OnReloadPower;
 import basemod.helpers.CardModifierManager;
+import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.actions.unique.LoseEnergyAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.localization.UIStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
 
 public abstract class AbstractReloadableCard extends AbstractClickableCard implements OnDiscardedCard {
+    private static final UIStrings strings = CardCrawlGame.languagePack.getUIString(M10RobotMod.makeID("Reloadable"));
+    public static final Color RELOADING_RED = Color.RED.cpy();
     public boolean needsReload;
 
     public AbstractReloadableCard(String id, String img, int cost, CardType type, CardColor color, CardRarity rarity, CardTarget target) {
@@ -35,9 +41,15 @@ public abstract class AbstractReloadableCard extends AbstractClickableCard imple
         }
     }
 
-    @Override
     public boolean canUse(AbstractPlayer p, AbstractMonster m) {
-        return super.canUse(p, m) && (isInAutoplay || !needsReload);
+        boolean canUse = super.canUse(p, m);
+        if (!canUse) {
+            return false;
+        } else if (needsReload && !isInAutoplay) {
+            canUse = false;
+            this.cantUseMessage = strings.TEXT[0];
+        }
+        return canUse;
     }
 
     public void useShot() {
