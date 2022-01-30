9d838991b43f0805fcdcc06043949f0e3e9ee394
Alison Turner <alisonmoons@gmail.com>
2022-01-29 20:37:15 -0400

    OverclockModifier now fixes magic number because resetAttributes can't be trusted.

--- TheM10Robot/src/main/java/M10Robot/cardModifiers/OverclockModifier.java
+++ TheM10Robot/src/main/java/M10Robot/cardModifiers/OverclockModifier.java
@@ -65,9 +65,19 @@ public class OverclockModifier extends AbstractCardModifier {
         if (card instanceof AbstractModdedCard) {
             ((AbstractModdedCard) card).secondMagicNumber = (int) (((AbstractModdedCard) card).baseSecondMagicNumber * (100F + rawPercent) / 100F);
             ((AbstractModdedCard) card).isSecondMagicNumberModified = ((AbstractModdedCard) card).secondMagicNumber != ((AbstractModdedCard) card).baseSecondMagicNumber;
-            //((AbstractModdedCard) card).thirdMagicNumber = (int) (((AbstractModdedCard) card).baseThirdMagicNumber * (100F + rawPercent) / 100F);
-            //((AbstractModdedCard) card).isThirdMagicNumberModified = ((AbstractModdedCard) card).thirdMagicNumber != ((AbstractModdedCard) card).baseThirdMagicNumber;
         }
+    }
+
+    @Override
+    public void onRemove(AbstractCard card) {
+        card.magicNumber = card.baseMagicNumber;
+        card.isMagicNumberModified = false;
+        if (card instanceof AbstractModdedCard) {
+            ((AbstractModdedCard) card).secondMagicNumber = ((AbstractModdedCard) card).baseSecondMagicNumber;
+            ((AbstractModdedCard) card).isSecondMagicNumberModified = false;
+        }
+        card.applyPowers();
+        card.initializeDescription();
     }
 
     @Override
