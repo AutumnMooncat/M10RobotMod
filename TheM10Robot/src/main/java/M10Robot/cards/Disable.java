b5d53747bfde7d317ad520dd9003a2188afe941f
Alison Turner <alisonmoons@gmail.com>
2022-01-31 19:13:37 -0400

    Disable no longer applies Stasis. Now applies -5 Str for the turn

--- TheM10Robot/src/main/java/M10Robot/cards/Disable.java
+++ TheM10Robot/src/main/java/M10Robot/cards/Disable.java
@@ -3,11 +3,12 @@ package M10Robot.cards;
 import M10Robot.M10RobotMod;
 import M10Robot.cards.abstractCards.AbstractDynamicCard;
 import M10Robot.characters.M10Robot;
-import M10Robot.powers.EMPPower;
 import com.evacipated.cardcrawl.mod.stslib.cards.interfaces.BranchingUpgradesCard;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.GainStrengthPower;
+import com.megacrit.cardcrawl.powers.StrengthPower;
 import com.megacrit.cardcrawl.powers.VulnerablePower;
 import com.megacrit.cardcrawl.powers.WeakPower;
 
@@ -32,26 +33,29 @@ public class Disable extends AbstractDynamicCard implements BranchingUpgradesCar
 
     private static final int COST = 1;
     private static final int UPGRADE_COST = 0;
-    private static final int BASE = 1;
-    private static final int EFFECT = 2;
-    private static final int UPGRADE_PLUS_EFFECT = 1;
+    private static final int DEBUFFS = 1;
+    private static final int STR_DOWN = 5;
+    private static final int UPGRADE_PLUS_INFO = 1;
 
     // /STAT DECLARATION/
 
 
     public Disable() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
-        magicNumber = baseMagicNumber = EFFECT;
-        secondMagicNumber = baseSecondMagicNumber = BASE;
+        magicNumber = baseMagicNumber = STR_DOWN;
+        secondMagicNumber = baseSecondMagicNumber = DEBUFFS;
         info = baseInfo = 0;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new ApplyPowerAction(m, p, new EMPPower(m, magicNumber)));
+        this.addToBot(new ApplyPowerAction(m, p, new StrengthPower(m, -this.magicNumber), -this.magicNumber));
+        if (m != null && !m.hasPower("Artifact")) {
+            this.addToBot(new ApplyPowerAction(m, p, new GainStrengthPower(m, this.magicNumber), this.magicNumber));
+        }
         this.addToBot(new ApplyPowerAction(m, p, new WeakPower(m, secondMagicNumber, false)));
-        if (upgraded && info > 0) {
+        if (info > 0) {
             this.addToBot(new ApplyPowerAction(m, p, new VulnerablePower(m, secondMagicNumber, false)));
         }
     }
@@ -75,6 +79,6 @@ public class Disable extends AbstractDynamicCard implements BranchingUpgradesCar
     }
 
     public void branchUpgrade() {
-        upgradeInfo(UPGRADE_PLUS_EFFECT);
+        upgradeInfo(UPGRADE_PLUS_INFO);
     }
 }
