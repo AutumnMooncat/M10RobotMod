94d9b04524f9e3bdbeff787bae3aebfe906b1c62
Alison Turner <alisonmoons@gmail.com>
2022-03-11 18:12:36 -0400

    Scrape Off now uses Rip and Tear vfx

--- TheM10Robot/src/main/java/M10Robot/actions/ScrapeOffAction.java
+++ TheM10Robot/src/main/java/M10Robot/actions/ScrapeOffAction.java
@@ -1,5 +1,6 @@
 package M10Robot.actions;
 
+import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.actions.utility.WaitAction;
@@ -8,6 +9,7 @@ import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.vfx.combat.FlashAtkImgEffect;
+import com.megacrit.cardcrawl.vfx.combat.RipAndTearEffect;
 
 public class ScrapeOffAction extends AbstractGameAction {
     private DamageInfo info;
@@ -26,7 +28,7 @@ public class ScrapeOffAction extends AbstractGameAction {
         } else {
             this.tickDuration();
             if (this.isDone) {
-                AbstractDungeon.effectList.add(new FlashAtkImgEffect(this.target.hb.cX, this.target.hb.cY, AttackEffect.SLASH_VERTICAL, false));
+                AbstractDungeon.effectsQueue.add(new RipAndTearEffect(this.target.hb.cX, this.target.hb.cY, Color.RED, Color.GOLD));
                 this.target.damage(this.info);
                 if (this.target.lastDamageTaken > 0) {
                     //this.addToTop(new ApplyPowerAction(this.source, this.source, new ComponentsPower(this.source, this.target.lastDamageTaken)));
