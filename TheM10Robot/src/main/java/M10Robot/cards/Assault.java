282db158c20baf9f69d09bec6e92b8796c59e014
Alison Turner <alisonmoons@gmail.com>
2022-03-11 10:47:59 -0400

    AssaultField now shuffles Assault into draw pile

--- TheM10Robot/src/main/java/M10Robot/cards/Assault.java
+++ TheM10Robot/src/main/java/M10Robot/cards/Assault.java
@@ -2,11 +2,15 @@ package M10Robot.cards;
 
 import M10Robot.M10RobotMod;
 import M10Robot.cards.abstractCards.AbstractDynamicCard;
+import M10Robot.cards.uniqueCards.UniqueCard;
 import M10Robot.characters.M10Robot;
 import M10Robot.powers.RecoilPower;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
-import com.megacrit.cardcrawl.actions.common.*;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -15,37 +19,37 @@ import com.megacrit.cardcrawl.vfx.combat.ExplosionSmallEffect;
 
 import static M10Robot.M10RobotMod.makeCardPath;
 
-public class AssaultField extends AbstractDynamicCard {
+public class Assault extends AbstractDynamicCard implements UniqueCard {
 
 
     // TEXT DECLARATION
 
-    public static final String ID = M10RobotMod.makeID(AssaultField.class.getSimpleName());
-    public static final String IMG = makeCardPath("AssaultField.png");
+    public static final String ID = M10RobotMod.makeID(Assault.class.getSimpleName());
+    public static final String IMG = makeCardPath("Assault.png");
 
     // /TEXT DECLARATION/
 
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardRarity RARITY = CardRarity.SPECIAL;
     private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
 
-    private static final int COST = 2;
+    private static final int COST = 0;
     private static final int DAMAGE = 4;
     private static final int UPGRADE_PLUS_DMG = 2;
-    private static final int CARDS = 3;
+    private static final int DRAW = 1;
 
     // /STAT DECLARATION/
 
-    public AssaultField() {
+    public Assault() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         baseDamage = damage = DAMAGE;
-        magicNumber = baseMagicNumber = CARDS;
-        cardsToPreview = new Assault();
+        magicNumber = baseMagicNumber = DRAW;
         isMultiDamage = true;
+        exhaust = true;
     }
 
     // Actions the card should do.
@@ -57,7 +61,7 @@ public class AssaultField extends AbstractDynamicCard {
             }
         }
         this.addToBot(new DamageAllEnemiesAction(p, multiDamage, damageTypeForTurn, AbstractGameAction.AttackEffect.NONE));
-        this.addToBot(new MakeTempCardInDrawPileAction(cardsToPreview, magicNumber, true, true));
+        this.addToBot(new DrawCardAction(p, magicNumber));
     }
 
     // Upgraded stats.
@@ -66,7 +70,6 @@ public class AssaultField extends AbstractDynamicCard {
         if (!upgraded) {
             upgradeName();
             upgradeDamage(UPGRADE_PLUS_DMG);
-            cardsToPreview.upgrade();
             initializeDescription();
         }
     }
