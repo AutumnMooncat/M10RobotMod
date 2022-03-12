20100dd2fc165a31f7c4923ffa16e946f8714fd3
Alison Turner <alisonmoons@gmail.com>
2022-03-11 23:35:10 -0400

    Split Beam should no longer be able to hit dead enemies

--- TheM10Robot/src/main/java/M10Robot/actions/SplitBeamAction.java
+++ TheM10Robot/src/main/java/M10Robot/actions/SplitBeamAction.java
@@ -1,31 +1,38 @@
 package M10Robot.actions;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.animations.VFXAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.actions.utility.SFXAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.vfx.combat.SmallLaserEffect;
 
 public class SplitBeamAction extends AbstractGameAction {
+    private static final float DURATION = Settings.ACTION_DUR_XFAST;
     private final AbstractCard card;
 
     public SplitBeamAction(AbstractCard card) {
         this.card = card;
+        this.actionType = ActionType.DAMAGE;
+        this.duration = DURATION;
     }
 
     public void update() {
-        this.target = AbstractDungeon.getMonsters().getRandomMonster();
-        if (this.target != null) {
-            this.card.calculateCardDamage((AbstractMonster)this.target);
-            this.addToTop(new DamageAction(this.target, new DamageInfo(AbstractDungeon.player, this.card.damage, this.card.damageTypeForTurn), AttackEffect.NONE));
-            this.addToTop(new VFXAction(new SmallLaserEffect(target.hb.cX, target.hb.cY, AbstractDungeon.player.hb.cX, AbstractDungeon.player.hb.cY)));
-            this.addToTop(new SFXAction("ATTACK_MAGIC_BEAM_SHORT", 0.5F));
+        if (duration == DURATION) {
+            this.target = AbstractDungeon.getMonsters().getRandomMonster(true);
+            if (this.target != null) {
+                this.card.calculateCardDamage((AbstractMonster)this.target);
+                CardCrawlGame.sound.play("ATTACK_MAGIC_BEAM_SHORT", 0.5F);
+                AbstractDungeon.effectList.add(new SmallLaserEffect(target.hb.cX, target.hb.cY, AbstractDungeon.player.hb.cX, AbstractDungeon.player.hb.cY));
+                DamageInfo di = new DamageInfo(AbstractDungeon.player, this.card.damage, this.card.damageTypeForTurn);
+                target.damage(di);
+                if (AbstractDungeon.getCurrRoom().monsters.areMonstersBasicallyDead()) {
+                    AbstractDungeon.actionManager.clearPostCombatActions();
+                }
+            }
         }
-
-        this.isDone = true;
+        tickDuration();
     }
 }
