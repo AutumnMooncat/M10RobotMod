9a3c765a5b76e794276b4191a64740c80ef7f79e
Alison Turner <alisonmoons@gmail.com>
2022-03-22 18:44:55 -0300

    Extract Action now supports consumer and follow-up actions

--- TheM10Robot/src/main/java/M10Robot/actions/ExtractAction.java
+++ TheM10Robot/src/main/java/M10Robot/actions/ExtractAction.java
@@ -11,17 +11,33 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
 public class ExtractAction extends AbstractGameAction {
-    private boolean random;
-    private int newCost;
-    private boolean setCost;
+    private final boolean random;
+    private final Consumer<AbstractCard> onExtract;
+    private final AbstractGameAction followAction;
     private static final float DURATION = Settings.ACTION_DUR_XFAST;
+    private static final ArrayList<AbstractCard> extractedCards = new ArrayList<>();
 
     public ExtractAction(int amount) {
-        this(amount, false);
+        this(amount, false, card -> {});
     }
 
     public ExtractAction(int amount, boolean random) {
+        this(amount, random, card -> {});
+    }
+
+    public ExtractAction(int amount, boolean random, Consumer<AbstractCard> onExtract) {
+        this(amount, random, onExtract, null);
+    }
+
+    public ExtractAction(int amount, boolean random, AbstractGameAction followAction) {
+        this(amount, random, card -> {}, followAction);
+    }
+
+    public ExtractAction(int amount, boolean random, Consumer<AbstractCard> onExtract, AbstractGameAction followAction) {
         this.random = random;
         this.actionType = ActionType.CARD_MANIPULATION;
         this.duration = DURATION;
@@ -37,21 +53,13 @@ public class ExtractAction extends AbstractGameAction {
             }
         }
         this.amount = amount;
+        this.onExtract = onExtract;
+        this.followAction = followAction;
     }
-
-    public ExtractAction(int amount, int newCost) {
-        this(amount, newCost, false);
-    }
-
-    public ExtractAction(int amount, int newCost, boolean random) {
-        this(amount, random);
-        this.newCost = newCost;
-        this.setCost = true;
-    }
-
 
     @Override
     public void update() {
+        extractedCards.clear();
         amount = Math.min(amount, AbstractDungeon.player.discardPile.size());
         if (amount > 0) {
             if (random) {
@@ -64,15 +72,18 @@ public class ExtractAction extends AbstractGameAction {
                 }
             }
         }
+        if (followAction != null) {
+            this.addToTop(followAction);
+        }
         this.isDone = true;
     }
 
     private void extract(AbstractCard c) {
         if (AbstractDungeon.player.hand.size() < BaseMod.MAX_HAND_SIZE) {
+            extractedCards.add(c);
+            onExtract.accept(c);
+
             AbstractDungeon.player.hand.addToHand(c);
-            if (this.setCost) {
-                c.setCostForTurn(this.newCost);
-            }
             AbstractDungeon.player.discardPile.removeCard(c);
 
             c.lighten(false);
