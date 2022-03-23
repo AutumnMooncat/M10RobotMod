2e713269664c216ab7ac458028704346f16f13c7
Alison Turner <alisonmoons@gmail.com>
2022-03-23 09:00:53 -0300

    Quick Repair now gives Block when played and when Extracted

--- TheM10Robot/src/main/java/M10Robot/cards/QuickRepair.java
+++ TheM10Robot/src/main/java/M10Robot/cards/QuickRepair.java
@@ -2,16 +2,17 @@ package M10Robot.cards;
 
 import M10Robot.M10RobotMod;
 import M10Robot.cards.abstractCards.AbstractDynamicCard;
+import M10Robot.cards.interfaces.OnExtractCard;
 import M10Robot.characters.M10Robot;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.PlatedArmorPower;
 
 import static M10Robot.M10RobotMod.makeCardPath;
 
-public class QuickRepair extends AbstractDynamicCard {
+public class QuickRepair extends AbstractDynamicCard implements OnExtractCard {
 
     /*
      * Wiki-page: https://github.com/daviscook477/BaseMod/wiki/Custom-Cards
@@ -35,9 +36,9 @@ public class QuickRepair extends AbstractDynamicCard {
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
 
-    private static final int COST = 0;
+    private static final int COST = 1;
     private static final int BLOCK = 6;
-    private static final int UPGRADE_PLUS_BLOCK = 4;
+    private static final int UPGRADE_PLUS_BLOCK = 2;
     private static final int PLATED = 3;
     private static final int UPGRADE_PLUS_PLATED = 2;
 
@@ -47,15 +48,12 @@ public class QuickRepair extends AbstractDynamicCard {
     public QuickRepair() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         block = baseBlock = BLOCK;
-        magicNumber = baseMagicNumber = PLATED;
-        exhaust = true;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         this.addToBot(new GainBlockAction(p, block));
-        this.addToBot(new ApplyPowerAction(p, p, new PlatedArmorPower(p, magicNumber)));
     }
 
     //Upgraded stats.
@@ -64,8 +62,15 @@ public class QuickRepair extends AbstractDynamicCard {
         if (!upgraded) {
             upgradeName();
             upgradeBlock(UPGRADE_PLUS_BLOCK);
-            upgradeMagicNumber(UPGRADE_PLUS_PLATED);
             initializeDescription();
         }
     }
+
+    @Override
+    public void onExtracted() {
+        this.addToBot(new GainBlockAction(AbstractDungeon.player, block));
+    }
+
+    @Override
+    public void onExtractCard(AbstractCard c) {}
 }
