bbda27258800ee56e1d8e66a8fd16cb0e7923b0b
Alison Turner <alisonmoons@gmail.com>
2022-03-24 17:40:32 -0300

    Split Beam now dies 3(4) damage 2 times, and increases the hits when Extracted

--- TheM10Robot/src/main/java/M10Robot/actions/SplitBeamAction.java
+++ TheM10Robot/src/main/java/M10Robot/actions/SplitBeamAction.java
@@ -1,8 +1,10 @@
 package M10Robot.actions;
 
+import com.badlogic.gdx.math.MathUtils;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -14,18 +16,25 @@ public class SplitBeamAction extends AbstractGameAction {
     private final AbstractCard card;
 
     public SplitBeamAction(AbstractCard card) {
+        this(card, null);
+    }
+
+    public SplitBeamAction(AbstractCard card, AbstractCreature target) {
         this.card = card;
+        this.target = target;
         this.actionType = ActionType.DAMAGE;
         this.duration = DURATION;
     }
 
     public void update() {
         if (duration == DURATION) {
-            this.target = AbstractDungeon.getMonsters().getRandomMonster(true);
+            if (this.target == null) {
+                this.target = AbstractDungeon.getMonsters().getRandomMonster(true);
+            }
             if (this.target != null) {
                 this.card.calculateCardDamage((AbstractMonster)this.target);
                 CardCrawlGame.sound.play("ATTACK_MAGIC_BEAM_SHORT", 0.5F);
-                AbstractDungeon.effectList.add(new SmallLaserEffect(target.hb.cX, target.hb.cY, AbstractDungeon.player.hb.cX, AbstractDungeon.player.hb.cY));
+                AbstractDungeon.effectList.add(new SmallLaserEffect(target.hb.cX + MathUtils.random(-50.0F * Settings.scale, 50.0F * Settings.scale), target.hb.cY + MathUtils.random(-50.0F * Settings.scale, 50.0F * Settings.scale), AbstractDungeon.player.hb.cX, AbstractDungeon.player.hb.cY));
                 DamageInfo di = new DamageInfo(AbstractDungeon.player, this.card.damage, this.card.damageTypeForTurn);
                 target.damage(di);
                 if (AbstractDungeon.getCurrRoom().monsters.areMonstersBasicallyDead()) {
--- TheM10Robot/src/main/java/M10Robot/cards/SplitBeam.java
+++ TheM10Robot/src/main/java/M10Robot/cards/SplitBeam.java
@@ -4,6 +4,7 @@ import M10Robot.M10RobotMod;
 import M10Robot.actions.SplitBeamAction;
 import M10Robot.cardModifiers.AimedModifier;
 import M10Robot.cards.abstractCards.AbstractDynamicCard;
+import M10Robot.cards.interfaces.OnExtractCard;
 import M10Robot.characters.M10Robot;
 import basemod.helpers.CardModifierManager;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
@@ -11,6 +12,7 @@ import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
 import com.megacrit.cardcrawl.actions.utility.SFXAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -19,7 +21,7 @@ import com.megacrit.cardcrawl.vfx.combat.SmallLaserEffect;
 
 import static M10Robot.M10RobotMod.makeCardPath;
 
-public class SplitBeam extends AbstractDynamicCard {
+public class SplitBeam extends AbstractDynamicCard implements OnExtractCard {
 
 
     // TEXT DECLARATION
@@ -33,13 +35,13 @@ public class SplitBeam extends AbstractDynamicCard {
     // STAT DECLARATION
 
     private static final CardRarity RARITY = CardRarity.COMMON;
-    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
 
     private static final int COST = 0;
     private static final int DAMAGE = 3;
-    private static final int UPGRADE_PLUS_DMG = 2;
+    private static final int UPGRADE_PLUS_DMG = 1;
     private static final int HITS = 2;
     private static final int UPGRADE_PLUS_HITS = 1;
 
@@ -49,6 +51,7 @@ public class SplitBeam extends AbstractDynamicCard {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         baseDamage = damage = DAMAGE;
         magicNumber = baseMagicNumber = HITS;
+        secondMagicNumber = baseSecondMagicNumber = UPGRADE_PLUS_HITS;
         //CardModifierManager.addModifier(this, new AimedModifier());
     }
 
@@ -56,7 +59,7 @@ public class SplitBeam extends AbstractDynamicCard {
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         for (int i = 0 ; i < magicNumber ; i++) {
-            this.addToBot(new SplitBeamAction(this));
+            this.addToBot(new SplitBeamAction(this, m));
         }
     }
 
@@ -65,9 +68,18 @@ public class SplitBeam extends AbstractDynamicCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            //upgradeDamage(UPGRADE_PLUS_DMG);
-            upgradeMagicNumber(UPGRADE_PLUS_HITS);
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            //upgradeMagicNumber(UPGRADE_PLUS_HITS);
             initializeDescription();
         }
     }
+
+    @Override
+    public void onExtracted() {
+        superFlash();
+        upgradeMagicNumber(secondMagicNumber);
+    }
+
+    @Override
+    public void onExtractCard(AbstractCard c) {}
 }
