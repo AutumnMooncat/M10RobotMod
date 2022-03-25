dd1f93e9c199b005f7f27ce270e45e792d6bdf39
Alison Turner <alisonmoons@gmail.com>
2022-03-25 12:27:22 -0300

    Seeker now deals damage and applies Lock-On. If the target already had Lock-On, it also draws.

--- TheM10Robot/src/main/java/M10Robot/cards/Seeker.java
+++ TheM10Robot/src/main/java/M10Robot/cards/Seeker.java
@@ -1,22 +1,23 @@
 package M10Robot.cards;
 
 import M10Robot.M10RobotMod;
-import M10Robot.actions.MultichannelAction;
-import M10Robot.cardModifiers.AimedModifier;
 import M10Robot.cards.abstractCards.AbstractDynamicCard;
 import M10Robot.characters.M10Robot;
-import M10Robot.orbs.SearchlightOrb;
-import basemod.helpers.CardModifierManager;
+import com.evacipated.cardcrawl.mod.stslib.cards.interfaces.BranchingUpgradesCard;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.LockOnPower;
 
 import static M10Robot.M10RobotMod.makeCardPath;
 
-public class Seeker extends AbstractDynamicCard {
+public class Seeker extends AbstractDynamicCard implements BranchingUpgradesCard {
 
 
     // TEXT DECLARATION
@@ -35,18 +36,22 @@ public class Seeker extends AbstractDynamicCard {
     public static final AbstractCard.CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
 
     private static final int COST = 1;
-    private static final int DAMAGE = 7;
-    private static final int UPGRADE_PLUS_DMG = 3;
-    private static final int ORBS = 1;
+    private static final int DAMAGE = 6;
+    private static final int UPGRADE_PLUS_DMG = 2;
+    private static final int EFFECT = 2;
+    private static final int UPGRADE_PLUS_EFFECT = 1;
+    private static final int CARDS = 1;
+    private static final int UPGRADE_PLUS_CARDS = 1;
 
     // /STAT DECLARATION/
 
     public Seeker() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         baseDamage = damage = DAMAGE;
-        magicNumber = baseMagicNumber = ORBS;
+        magicNumber = baseMagicNumber = EFFECT;
+        secondMagicNumber = baseSecondMagicNumber = CARDS;
         showEvokeValue = true;
-        CardModifierManager.addModifier(this, new AimedModifier());
+        //CardModifierManager.addModifier(this, new AimedModifier());
     }
 
     // Actions the card should do.
@@ -54,16 +59,38 @@ public class Seeker extends AbstractDynamicCard {
     public void use(AbstractPlayer p, AbstractMonster m) {
         //this.addToBot(new AttackDamageRandomEnemyAction(this, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
         this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
-        this.addToBot(new MultichannelAction(new SearchlightOrb(), magicNumber));
+        this.addToBot(new ApplyPowerAction(m, p, new LockOnPower(m, magicNumber)));
+        if (m.hasPower(LockOnPower.POWER_ID)) {
+            this.addToBot(new DrawCardAction(secondMagicNumber));
+        }
     }
 
-    // Upgraded stats.
+    public void triggerOnGlowCheck() {
+        this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
+        if (AbstractDungeon.getMonsters().monsters.stream().anyMatch(m -> m.hasPower(LockOnPower.POWER_ID))) {
+            this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
+        }
+    }
+
+    //Upgraded stats.
     @Override
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeDamage(UPGRADE_PLUS_DMG);
+            if (isBranchUpgrade()) {
+                branchUpgrade();
+            } else {
+                baseUpgrade();
+            }
             initializeDescription();
         }
+    }
+
+    public void baseUpgrade() {
+        upgradeDamage(UPGRADE_PLUS_DMG);
+    }
+
+    public void branchUpgrade() {
+        upgradeSecondMagicNumber(UPGRADE_PLUS_CARDS);
     }
 }
