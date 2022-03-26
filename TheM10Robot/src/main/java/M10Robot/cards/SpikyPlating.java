7cd168478a99d42071bb36e7ff42bbd4c4a1de84
Alison Turner <alisonmoons@gmail.com>
2022-03-26 13:14:19 -0300

    Spiky Plating now Channels 2 Searchlight

--- TheM10Robot/src/main/java/M10Robot/cards/SpikyPlating.java
+++ TheM10Robot/src/main/java/M10Robot/cards/SpikyPlating.java
@@ -1,9 +1,11 @@
 package M10Robot.cards;
 
 import M10Robot.M10RobotMod;
+import M10Robot.actions.MultichannelAction;
 import M10Robot.cards.abstractCards.AbstractDynamicCard;
 import M10Robot.characters.M10Robot;
 import M10Robot.cutStuff.powers.SpikyPlatingPower;
+import M10Robot.orbs.SearchlightOrb;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -32,6 +34,7 @@ public class SpikyPlating extends AbstractDynamicCard {
     private static final int UPGRADE_COST = 0;
     private static final int THORNS = 1;
     private static final int UPGRADE_PLUS_THORNS = 1;
+    private static final int ORBS = 2;
 
     // /STAT DECLARATION/
 
@@ -39,12 +42,15 @@ public class SpikyPlating extends AbstractDynamicCard {
     public SpikyPlating() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         magicNumber = baseMagicNumber = THORNS;
+        secondMagicNumber = baseSecondMagicNumber = ORBS;
+        showEvokeValue = true;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         this.addToBot(new ApplyPowerAction(p, p, new SpikyPlatingPower(p, magicNumber)));
+        this.addToBot(new MultichannelAction(new SearchlightOrb(), secondMagicNumber));
     }
 
     //Upgraded stats.
