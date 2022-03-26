3daec10c3419294f2e8ad5c6039b9105b45c67c1
Alison Turner <alisonmoons@gmail.com>
2022-03-26 13:29:41 -0300

    Vine Whip now named Lash. Cost increased from 0 -> 1. Damage increased 5(7) -> 6(9). Now Channels 1 Searchlight instead of giving Spikes.

--- TheM10Robot/src/main/java/M10Robot/cards/ThornWhip.java
+++ TheM10Robot/src/main/java/M10Robot/cards/ThornWhip.java
@@ -1,9 +1,11 @@
 package M10Robot.cards;
 
 import M10Robot.M10RobotMod;
+import M10Robot.actions.MultichannelAction;
 import M10Robot.cardModifiers.SpikyModifier;
 import M10Robot.cards.abstractCards.AbstractDynamicCard;
 import M10Robot.characters.M10Robot;
+import M10Robot.orbs.SearchlightOrb;
 import M10Robot.powers.SpikesPower;
 import basemod.helpers.CardModifierManager;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
@@ -33,10 +35,10 @@ public class ThornWhip extends AbstractDynamicCard {
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
 
-    private static final int COST = 0;
-    private static final int DAMAGE = 5;
-    private static final int UPGRADE_PLUS_DMG = 2;
-    private static final int SPIKES = 2;
+    private static final int COST = 1;
+    private static final int DAMAGE = 6;
+    private static final int UPGRADE_PLUS_DMG = 3;
+    private static final int SPIKES = 1;
     private static final int UPGRADE_PLUS_SPIKES = 1;
 
     // /STAT DECLARATION/
@@ -45,6 +47,7 @@ public class ThornWhip extends AbstractDynamicCard {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         baseDamage = damage = DAMAGE;
         magicNumber = baseMagicNumber = SPIKES;
+        showEvokeValue = true;
         //CardModifierManager.addModifier(this, new SpikyModifier());
     }
 
@@ -52,7 +55,8 @@ public class ThornWhip extends AbstractDynamicCard {
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
-        this.addToBot(new ApplyPowerAction(p, p, new SpikesPower(p, magicNumber)));
+        //this.addToBot(new ApplyPowerAction(p, p, new SpikesPower(p, magicNumber)));
+        this.addToBot(new MultichannelAction(new SearchlightOrb(), magicNumber));
     }
 
     // Upgraded stats.
@@ -61,7 +65,7 @@ public class ThornWhip extends AbstractDynamicCard {
         if (!upgraded) {
             upgradeName();
             upgradeDamage(UPGRADE_PLUS_DMG);
-            upgradeMagicNumber(UPGRADE_PLUS_SPIKES);
+            //upgradeMagicNumber(UPGRADE_PLUS_SPIKES);
             initializeDescription();
         }
     }
