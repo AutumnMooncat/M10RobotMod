eab6ea5ac8a3728644ced10d472bb19dd8799108
Alison Turner <alisonmoons@gmail.com>
2022-03-27 13:16:28 -0300

    Added Analytics - Whenever you Extract a Card, Overclock it

---
+++ TheM10Robot/src/main/java/M10Robot/cards/Analytics.java
@@ -0,0 +1,65 @@
+package M10Robot.cards;
+
+import M10Robot.M10RobotMod;
+import M10Robot.cards.abstractCards.AbstractDynamicCard;
+import M10Robot.characters.M10Robot;
+import M10Robot.powers.ExtrapolatePower;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static M10Robot.M10RobotMod.makeCardPath;
+
+public class Analytics extends AbstractDynamicCard {
+
+    /*
+     * Wiki-page: https://github.com/daviscook477/BaseMod/wiki/Custom-Cards
+     *
+     * Defend Gain 5 (8) block.
+     */
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = M10RobotMod.makeID(Analytics.class.getSimpleName());
+    public static final String IMG = makeCardPath("Analytics.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.POWER;
+    public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int UPGRADE_COST = 0;
+    private static final int EVOKES = 1;
+    private static final int UPGRADE_PLUS_EVOKES = 1;
+
+    // /STAT DECLARATION/
+
+
+    public Analytics() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        magicNumber = baseMagicNumber = EVOKES;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new ApplyPowerAction(p, p, new ExtrapolatePower(p, magicNumber)));
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBaseCost(UPGRADE_COST);
+            initializeDescription();
+        }
+    }
+}
--- TheM10Robot/src/main/java/M10Robot/powers/ExtrapolatePower.java
+++ TheM10Robot/src/main/java/M10Robot/powers/ExtrapolatePower.java
@@ -1,24 +1,21 @@
 package M10Robot.powers;
 
 import M10Robot.M10RobotMod;
-import M10Robot.actions.EvokelessEvocationAction;
+import M10Robot.actions.OverclockCardAction;
+import M10Robot.powers.interfaces.OnExtractPower;
 import basemod.interfaces.CloneablePowerInterface;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.megacrit.cardcrawl.actions.defect.AnimateOrbAction;
 import com.megacrit.cardcrawl.actions.defect.EvokeOrbAction;
 import com.megacrit.cardcrawl.actions.defect.EvokeWithoutRemovingOrbAction;
-import com.megacrit.cardcrawl.actions.unique.MulticastAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.helpers.FontHelper;
 import com.megacrit.cardcrawl.localization.PowerStrings;
-import com.megacrit.cardcrawl.orbs.AbstractOrb;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 
-public class AutoCasterPower extends AbstractPower implements CloneablePowerInterface {
+public class ExtrapolatePower extends AbstractPower implements CloneablePowerInterface, OnExtractPower {
 
-    public static final String POWER_ID = M10RobotMod.makeID("AutoCasterPower");
+    public static final String POWER_ID = M10RobotMod.makeID("ExtrapolatePower");
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
@@ -28,15 +25,12 @@ public class AutoCasterPower extends AbstractPower implements CloneablePowerInte
     //private static final Texture tex84 = TextureLoader.getTexture(makePowerPath("placeholder_power84.png"));
     //private static final Texture tex32 = TextureLoader.getTexture(makePowerPath("placeholder_power32.png"));
 
-    public AutoCasterPower(AbstractCreature owner, int amount) {
+    public ExtrapolatePower(AbstractCreature owner, int amount) {
         this.name = NAME;
         this.ID = POWER_ID;
         this.owner = owner;
         this.amount = amount;
-        this.priority = Short.MAX_VALUE;
-
         this.type = PowerType.BUFF;
-        this.isTurnBased = false;
 
         // We load those txtures here.
         //this.loadRegion("cExplosion");
@@ -68,8 +62,18 @@ public class AutoCasterPower extends AbstractPower implements CloneablePowerInte
 
     @Override
     public AbstractPower makeCopy() {
-        return new AutoCasterPower(owner, amount);
+        return new ExtrapolatePower(owner, amount);
     }
 
 
+    @Override
+    public int modifyExtractAmount(int amount) {
+        return amount;
+    }
+
+    @Override
+    public void onExtractCard(AbstractCard c) {
+        flash();
+        this.addToBot(new OverclockCardAction(c, amount));
+    }
 }
