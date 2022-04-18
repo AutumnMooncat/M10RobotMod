eb9dff23c8f462461b7871b5000f122bcdd06aea
Alison Turner <alisonmoons@gmail.com>
2022-04-18 10:28:25 -0300

    Reroute now has 4(6) scaling, was 3(4)

--- TheM10Robot/src/main/java/M10Robot/cards/Reroute.java
+++ TheM10Robot/src/main/java/M10Robot/cards/Reroute.java
@@ -34,8 +34,8 @@ public class Reroute extends AbstractDynamicCard implements OnOverclockCard {
     private static final int DAMAGE = 6;
     private static final int UPGRADE_PLUS_DAMAGE = 2;
     private static final int EXTRACT = 1;
-    private static final int BOOST = 3;
-    private static final int UPGRADE_PLUS_BOOST = 1;
+    private static final int BOOST = 4;
+    private static final int UPGRADE_PLUS_BOOST = 2;
 
     // /STAT DECLARATION/
 
