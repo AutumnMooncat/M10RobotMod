fbd2f3c12fb389c77d45412f17f35f2ddd6f0327
Alison Turner <alisonmoons@gmail.com>
2022-05-21 12:12:46 -0300

    Byte/Nibble now swappable, Bite now deals 8 damage and Channels 2 Bits. Nibble now gives 4 Block and deals 4 damage. Both upgrade to AOE.

--- TheM10Robot/src/main/java/M10Robot/cards/Byte.java
+++ TheM10Robot/src/main/java/M10Robot/cards/Byte.java
@@ -3,20 +3,28 @@ package M10Robot.cards;
 import M10Robot.M10RobotMod;
 import M10Robot.actions.MultichannelAction;
 import M10Robot.cards.abstractCards.AbstractDynamicCard;
+import M10Robot.cards.abstractCards.AbstractSwappableCard;
 import M10Robot.cards.tokenCards.Nibble;
 import M10Robot.characters.M10Robot;
 import M10Robot.orbs.BitOrb;
+import basemod.helpers.TooltipInfo;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
 import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
 import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.helpers.GameDictionary;
+import com.megacrit.cardcrawl.helpers.TipHelper;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static M10Robot.M10RobotMod.makeCardPath;
 
-public class Byte extends AbstractDynamicCard {
+public class Byte extends AbstractSwappableCard {
 
     // TEXT DECLARATION
 
@@ -35,32 +43,47 @@ public class Byte extends AbstractDynamicCard {
 
     private static final int COST = 2;
     private static final int DAMAGE = 8;
-    private static final int CARDS = 2;
-    private static final int ORBS = 1;
+    private static final int ORBS = 2;
     private static final int UPGRADE_PLUS_ORBS = 1;
 
-    // /STAT DECLARATION/
+    protected static ArrayList<TooltipInfo> toolTips;
 
+    // /STAT DECLARATION/
     public Byte() {
+        this(null);
+    }
+
+    public Byte(AbstractSwappableCard linkedCard) {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         baseDamage = damage = DAMAGE;
-        secondMagicNumber = baseSecondMagicNumber = CARDS;
         magicNumber = baseMagicNumber = ORBS;
+        info = baseInfo = 0;
         showEvokeValue = true;
-        cardsToPreview = new Nibble();
-        baseInfo = info = 0;
+        if (linkedCard == null) {
+            setLinkedCard(new Nibble(this));
+        } else {
+            setLinkedCard(linkedCard);
+        }
+    }
+
+    @Override
+    public List<TooltipInfo> getCustomTooltips() {
+        if (toolTips == null) {
+            toolTips = new ArrayList<>();
+            toolTips.add(new TooltipInfo(TipHelper.capitalize(GameDictionary.BLOCK.NAMES[0]), GameDictionary.keywords.get(GameDictionary.BLOCK.NAMES[0])));
+        }
+        return toolTips;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE));
-        this.addToBot(new MultichannelAction(new BitOrb(), magicNumber));
         if (info == 0) {
-            this.addToBot(new MakeTempCardInDrawPileAction(cardsToPreview, secondMagicNumber, true, true));
+            this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE));
         } else {
-            this.addToBot(new MakeTempCardInHandAction(cardsToPreview, secondMagicNumber));
+            this.addToBot(new DamageAllEnemiesAction(p, multiDamage, damageTypeForTurn, AbstractGameAction.AttackEffect.FIRE));
         }
+        this.addToBot(new MultichannelAction(new BitOrb(), magicNumber));
     }
 
     // Upgraded stats.
@@ -70,7 +93,10 @@ public class Byte extends AbstractDynamicCard {
             upgradeName();
             //upgradeMagicNumber(UPGRADE_PLUS_ORBS);
             upgradeInfo(1);
+            isMultiDamage = true;
+            target = CardTarget.ALL_ENEMY;
             initializeDescription();
+            super.upgrade();
         }
     }
 }
--- TheM10Robot/src/main/java/M10Robot/cards/tokenCards/Nibble.java
+++ TheM10Robot/src/main/java/M10Robot/cards/tokenCards/Nibble.java
@@ -2,22 +2,34 @@ package M10Robot.cards.tokenCards;
 
 import M10Robot.M10RobotMod;
 import M10Robot.actions.BitAttackAction;
+import M10Robot.cards.Byte;
+import M10Robot.cards.DefenseDown;
 import M10Robot.cards.abstractCards.AbstractDynamicCard;
+import M10Robot.cards.abstractCards.AbstractSwappableCard;
+import M10Robot.cards.uniqueCards.UniqueCard;
 import M10Robot.characters.M10Robot;
 import M10Robot.orbs.BitOrb;
+import basemod.BaseMod;
+import basemod.helpers.TooltipInfo;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.GameDictionary;
+import com.megacrit.cardcrawl.helpers.TipHelper;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.orbs.AbstractOrb;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static M10Robot.M10RobotMod.makeCardPath;
 
-public class Nibble extends AbstractDynamicCard implements TokenCard {
+public class Nibble extends AbstractSwappableCard implements UniqueCard {
 
     // TEXT DECLARATION
 
@@ -29,7 +41,7 @@ public class Nibble extends AbstractDynamicCard implements TokenCard {
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.SPECIAL;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = M10Robot.Enums.GREEN_SPRING_CARD_COLOR;
@@ -39,36 +51,60 @@ public class Nibble extends AbstractDynamicCard implements TokenCard {
     private static final int MONCH = 1;
     private static final int UPGRADE_PLUS_MONCH = 1;
 
+    protected static ArrayList<TooltipInfo> toolTips;
+
     // /STAT DECLARATION/
 
     public Nibble() {
+        this(null);
+    }
+
+    public Nibble(AbstractSwappableCard linkedCard) {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         baseDamage = damage = EFFECT;
         baseBlock = block = EFFECT;
-        magicNumber = baseMagicNumber = MONCH;
-        //CardModifierManager.addModifier(this, new AimedModifier());
+        info = baseInfo = 0;
+        if (linkedCard == null) {
+            setLinkedCard(new Byte(this));
+        } else {
+            setLinkedCard(linkedCard);
+        }
+    }
+
+    @Override
+    public List<TooltipInfo> getCustomTooltips() {
+        if (toolTips == null) {
+            toolTips = new ArrayList<>();
+            toolTips.add(new TooltipInfo(TipHelper.capitalize(GameDictionary.CHANNEL.NAMES[0]), GameDictionary.keywords.get(GameDictionary.CHANNEL.NAMES[0])));
+            toolTips.add(new TooltipInfo(BaseMod.getKeywordTitle("m10robot:bit"), BaseMod.getKeywordDescription("m10robot:bit")));
+        }
+        return toolTips;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         this.addToBot(new GainBlockAction(p, block));
-        this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE));
-        for (AbstractOrb o : p.orbs) {
+        if (info == 0) {
+            this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE));
+        } else {
+            this.addToBot(new DamageAllEnemiesAction(p, multiDamage, damageTypeForTurn, AbstractGameAction.AttackEffect.FIRE));
+        }
+        /*for (AbstractOrb o : p.orbs) {
             if (o instanceof BitOrb) {
                 for (int i = 0 ; i < magicNumber ; i++) {
                     this.addToBot(new BitAttackAction((BitOrb) o, p, m));
                 }
             }
-        }
+        }*/
     }
 
-    public void triggerOnGlowCheck() {
+    /*public void triggerOnGlowCheck() {
         this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
         if (AbstractDungeon.player.orbs.stream().anyMatch(o -> o instanceof BitOrb)) {
             this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
         }
-    }
+    }*/
 
     // Upgraded stats.
     @Override
@@ -76,7 +112,11 @@ public class Nibble extends AbstractDynamicCard implements TokenCard {
         if (!upgraded) {
             upgradeName();
             upgradeMagicNumber(UPGRADE_PLUS_MONCH);
+            upgradeInfo(1);
+            isMultiDamage = true;
+            target = CardTarget.ALL_ENEMY;
             initializeDescription();
+            super.upgrade();
         }
     }
 }
